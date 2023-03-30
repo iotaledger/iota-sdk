01eb6e6b7386c38d724038e6fe9565df8c175c2e
Thibault Martinez <thibault@iota.org>
2023-03-30 17:20:55 +0200

    CLI: improve help and add autocompletion (#149)
    * AccountManagerCommand help
    
    * Move examples to field docs
    
    * Pretty print sync and balance
    
    * Add autocompletion
    
    * println_log_error
    
    * Some more help
    
    * More help
    
    * Last helps
    
    * fmt
    
    * Update cli/src/command/account.rs
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

---
+++ cli/src/account_completion.rs
@@ -0,0 +1,62 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use dialoguer::Completion;
+
+pub(crate) struct AccountCompletion<'a> {
+    options: [&'a str; 33],
+}
+
+pub(crate) const ACCOUNT_COMPLETION: AccountCompletion = AccountCompletion {
+    options: [
+        "addresses",
+        "balance",
+        "burn-native-token",
+        "burn-nft",
+        "claim",
+        "consolidate",
+        "create-alias-output",
+        "decrease-native-token-supply",
+        "destroy-alias",
+        "destroy-foundry",
+        "exit",
+        "faucet",
+        "increase-native-token-supply",
+        "mint-native-token",
+        "mint-nft",
+        "new-address",
+        "output",
+        "outputs",
+        "send",
+        "send-micro",
+        "send-native-token",
+        "send-nft",
+        "sync",
+        "transactions",
+        "unspent-outputs",
+        "vote",
+        "stop-participating",
+        "participation-overview",
+        "voting-power",
+        "increase-voting-power",
+        "decrease-voting-power",
+        "voting-output",
+        "help",
+    ],
+};
+
+impl<'a> Completion for AccountCompletion<'a> {
+    fn get(&self, input: &str) -> Option<String> {
+        let matches = self
+            .options
+            .iter()
+            .filter(|option| option.starts_with(input))
+            .collect::<Vec<_>>();
+
+        if matches.len() == 1 {
+            Some(matches[0].to_string())
+        } else {
+            None
+        }
+    }
+}
