9e13978f1eb5154d8a96db33d0b238e6aec9e147
/alex/ <al3k5@protonmail.com>
2023-04-05 12:46:01 +0200

    add todo

--- sdk/src/wallet/bindings/java/lib/native/src/lib.rs
+++ sdk/src/wallet/bindings/java/lib/native/src/lib.rs
@@ -76,6 +76,7 @@ macro_rules! env_assert {
 }
 
 // This keeps rust from "mangling" the name and making it unique for this crate.
+// TODO: add safety doc
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "system" fn Java_org_iota_api_NativeApi_initLogger(
@@ -90,6 +91,7 @@ pub unsafe extern "system" fn Java_org_iota_api_NativeApi_initLogger(
 }
 
 // This keeps rust from "mangling" the name and making it unique for this crate.
+// TODO: add safety doc
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "system" fn Java_org_iota_api_NativeApi_createMessageHandler(
@@ -151,6 +153,7 @@ pub extern "system" fn Java_org_iota_api_NativeApi_destroyHandle(mut env: JNIEnv
 }
 
 // This keeps rust from "mangling" the name and making it unique for this crate.
+// TODO: add safety doc
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "system" fn Java_org_iota_api_NativeApi_sendMessage(
