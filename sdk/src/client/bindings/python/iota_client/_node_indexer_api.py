496a6c0665b779922b7937458d506710f844c6d9
Dr-Electron <dr-electr0n@protonmail.com>
2023-04-06 09:29:35 +0200

    Implement basic_output_ids in pythonic way (#168)
    * Add QueryParameter class
    
    * Update query parameters

--- sdk/src/client/bindings/python/iota_client/_node_indexer_api.py
+++ sdk/src/client/bindings/python/iota_client/_node_indexer_api.py
@@ -1,4 +1,6 @@
 from iota_client._base_api import BaseAPI
+from dataclasses import dataclass
+import humps
 
 
 class NodeIndexerAPI(BaseAPI):
@@ -6,6 +8,9 @@ class NodeIndexerAPI(BaseAPI):
     def basic_output_ids(self, query_parameters):
         """Fetch basic output IDs.
         """
+
+        query_parameters = humps.camelize(query_parameters.as_dict())
+
         return self.send_message('basicOutputIds', {
             'queryParameters': query_parameters,
         })
@@ -13,6 +18,9 @@ class NodeIndexerAPI(BaseAPI):
     def alias_output_ids(self, query_parameters):
         """Fetch alias output IDs.
         """
+
+        query_parameters = humps.camelize(query_parameters.as_dict())
+
         return self.send_message('aliasOutputIds', {
             'queryParameters': query_parameters,
         })
@@ -27,6 +35,9 @@ class NodeIndexerAPI(BaseAPI):
     def nft_output_ids(self, query_parameters):
         """Fetch NFT output IDs.
         """
+
+        query_parameters = humps.camelize(query_parameters.as_dict())
+
         return self.send_message('nftOutputIds', {
             'queryParameters': query_parameters,
         })
@@ -41,6 +52,9 @@ class NodeIndexerAPI(BaseAPI):
     def foundry_output_ids(self, query_parameters):
         """Fetch foundry Output IDs.
         """
+
+        query_parameters = humps.camelize(query_parameters.as_dict())
+
         return self.send_message('foundryOutputIds', {
             'queryParameters': query_parameters,
         })
@@ -51,3 +65,32 @@ class NodeIndexerAPI(BaseAPI):
         return self.send_message('foundryOutputId', {
             'foundryId': foundry_id
         })
+
+    @dataclass
+    class QueryParameter:
+        address: str = None
+        alias_address: str = None
+        created_after: int = None
+        created_before: int = None
+        cursor: str = None
+        expiration_return_address: str = None
+        expires_after: int = None
+        expires_before: int = None
+        governor: str = None
+        has_expiration: bool = None
+        has_native_tokens: bool = None
+        has_storage_deposit_return: bool = None
+        has_timelock: bool = None
+        issuer: str = None
+        max_native_token_count: int = None
+        min_native_token_count: int = None
+        page_size: int = None
+        sender: str = None
+        state_controller: str = None
+        storage_deposit_return_address: str = None
+        tag: str = None
+        timelocked_after: int = None
+        timelocked_before: int = None
+
+        def as_dict(self):
+            return [{k: v} for k, v in self.__dict__.items() if v != None]
