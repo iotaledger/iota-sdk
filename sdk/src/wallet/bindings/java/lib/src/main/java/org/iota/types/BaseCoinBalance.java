aa66592fcd511aa154938a09ee96b02ec427d140
Thibault Martinez <thibault@iota.org>
2023-04-10 16:48:21 +0200

    Java bindings

--- sdk/src/wallet/bindings/java/lib/src/main/java/org/iota/types/BaseCoinBalance.java
+++ sdk/src/wallet/bindings/java/lib/src/main/java/org/iota/types/BaseCoinBalance.java
@@ -14,8 +14,10 @@ public class BaseCoinBalance extends AbstractObject {
     private long total;
     /// Balance that can currently be spent
     private long available;
+    /// Voting power
+    private long voting_power;
 
-    public BaseCoinBalance(long total, long available) {
+    public BaseCoinBalance(long total, long available, long voting_power) {
         this.total = total;
         this.available = available;
     }
@@ -27,6 +29,10 @@ public class BaseCoinBalance extends AbstractObject {
     public long getAvailable() {
         return available;
     }
+
+    public long getVotingPower() {
+        return voting_power;
+    }
 }
 
 class BaseCoinBalanceAdapter implements JsonDeserializer<BaseCoinBalance>, JsonSerializer<BaseCoinBalance> {
@@ -36,14 +42,16 @@ class BaseCoinBalanceAdapter implements JsonDeserializer<BaseCoinBalance>, JsonS
 
         long total = Long.parseLong(json.getAsJsonObject().get("total").getAsString());
         long available = Long.parseLong(json.getAsJsonObject().get("available").getAsString());
+        long voting_power = Long.parseLong(json.getAsJsonObject().get("votingPower").getAsString());
 
-        return new BaseCoinBalance(total, available);
+        return new BaseCoinBalance(total, available, voting_power);
     }
 
     public JsonElement serialize(BaseCoinBalance src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject o = new JsonObject();
         o.addProperty("total", String.valueOf(src.getTotal()));
         o.addProperty("available", String.valueOf(src.getAvailable()));
+        o.addProperty("votingPower", String.valueOf(src.getVotingPower()));
         return o;
     }
 }
