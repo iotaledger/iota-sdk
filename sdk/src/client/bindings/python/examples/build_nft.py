a9a65a78982717a3fd71a42e129ec9be89a8b83f
Dr-Electron <dr-electr0n@protonmail.com>
2023-04-13 15:17:50 +0200

    Implement output builders in pythonic way (#27)
    * Implement build_*_output in pythonic way
    
    * Fix amount
    
    * Remove __default
    
    * Add docstrings
    
    * Update examples
    
    * Add changelog
    
    * Update sdk/src/client/bindings/python/CHANGELOG.md
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Update sdk/src/client/bindings/python/iota_client/client.py
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Update sdk/src/client/bindings/python/iota_client/client.py
    
    * Update sdk/src/client/bindings/python/CHANGELOG.md
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Add Address wrapper classes
    
    * Update sdk/src/client/bindings/python/iota_client/client.py
    
    * Add UnlockCondition wrapper classes
    
    * Add Feature wrapper classes
    
    * Harmonise examples
    
    * Missed two
    
    ---------
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- sdk/src/client/bindings/python/examples/build_nft.py
+++ sdk/src/client/bindings/python/examples/build_nft.py
@@ -1,4 +1,4 @@
-from iota_client import IotaClient
+from iota_client import *
 import json
 
 # Create an IotaClient instance
@@ -19,52 +19,20 @@ tip_27_immutable_metadata = {
 # Build NFT output
 nft_output = client.build_nft_output(
     unlock_conditions=[
-        {
-            "type": 0,
-            "address": {
-                "type": 0,
-                "pubKeyHash": hexAddress,
-            },
-        },
+        AddressUnlockCondition(Ed25519Address(hexAddress))
     ],
     # NftId needs to be null the first time
-    nft_id="0x0000000000000000000000000000000000000000000000000000000000000000",
+    nft_id='0x0000000000000000000000000000000000000000000000000000000000000000',
     immutable_features=[
-        {
-            # issuer feature
-            'type': 1,
-            'address': {
-                'type': 0,
-                'pubKeyHash': hexAddress,
-            },
-        },
-        {
-            # metadata feature
-            'type': 2,
-            'data': '0x'+json.dumps(tip_27_immutable_metadata, separators=(',', ':')).encode("utf-8").hex(),
-        },
+        IssuerFeature(Ed25519Address(hexAddress)),
+        MetadataFeature('0x'+json.dumps(tip_27_immutable_metadata, separators=(',', ':')).encode('utf-8').hex())
     ],
     features=[
-        {
-            # sender feature
-            'type': 0,
-            'address': {
-                'type': 0,
-                'pubKeyHash': hexAddress,
-            },
-        },
-        {
-            # metadata feature
-            'type': 2,
-            'data': '0x'+'mutable metadata'.encode("utf-8").hex(),
-        },
-        {
-            # tag feature
-            'type': 3,
-            'tag': '0x'+'my tag'.encode("utf-8").hex(),
-        }
+        SenderFeature(Ed25519Address(hexAddress)),
+        MetadataFeature('0x'+'mutable metadata'.encode('utf-8').hex()),
+        TagFeature('0x'+'my tag'.encode("utf-8").hex())
     ]
 )
 
 # Print the output
-print(json.dumps(nft_output, indent=2))
+print(json.dumps(nft_output, indent=4))
