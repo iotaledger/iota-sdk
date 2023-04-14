ae11b2c347744473c2e00a8079051377e3936fb5
Thoralf Müller <thoralf.mue@gmail.com>
2023-04-14 12:19:21 +0200

    Update secret_manager test

--- bindings/core/tests/secrets_manager.rs
+++ bindings/core/tests/secrets_manager.rs
@@ -1,12 +1,14 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_sdk::client::{api::GetAddressesBuilderOptions, generate_mnemonic, secret::SecretManager};
+use iota_sdk::client::{api::GetAddressesBuilderOptions, secret::SecretManager};
 use iota_sdk_bindings_core::{call_secret_manager_method, Response, Result, SecretManagerMethod};
 
 #[tokio::test]
 async fn generate_addresses() -> Result<()> {
-    let mut secret_manager = SecretManager::try_from_mnemonic(&generate_mnemonic()?)?;
+    let mut secret_manager = SecretManager::try_from_mnemonic(
+        "endorse answer radar about source reunion marriage tag sausage weekend frost daring base attack because joke dream slender leisure group reason prepare broken river",
+    )?;
 
     let method = SecretManagerMethod::GenerateAddresses {
         options: GetAddressesBuilderOptions::default(),
@@ -14,7 +16,10 @@ async fn generate_addresses() -> Result<()> {
 
     let response = call_secret_manager_method(&mut secret_manager, method).await;
     match response {
-        Response::GeneratedAddresses(addresses) => println!("{addresses:?}"),
+        Response::GeneratedAddresses(addresses) => assert_eq!(
+            addresses[0],
+            "rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy"
+        ),
         _ => panic!("Unexpected response type"),
     };
 
