1b3c0b54161ac6ae874a8c880fa4d3b43944a85b
Dr-Electron <dr-electr0n@protonmail.com>
2023-04-17 13:43:28 +0200

    Use named arg

--- sdk/src/client/bindings/python/examples/08_data_block.py
+++ sdk/src/client/bindings/python/examples/08_data_block.py
@@ -5,5 +5,5 @@ client = IotaClient(nodes = ['https://api.testnet.shimmer.network'])
 
 # Create and post a block with a tagged data payload. Data is 'hello' hex encoded
 block = client.build_and_post_block(
-    None, tag='0x68656c6c6f', data='0x68656c6c6f')
+    secret_manager=None, tag='0x68656c6c6f', data='0x68656c6c6f')
 print(f'{block}')
