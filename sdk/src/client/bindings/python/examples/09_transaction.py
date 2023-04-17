7a4b8b3e94c1738058c68261415c81a689e8eeab
Dr-Electron <dr-electr0n@protonmail.com>
2023-04-17 16:18:09 +0200

    Implement build_and_post_block in pythonic way (#169)
    * Implement build_and_post_block in pythonic way
    
    * Fix rebase mess
    
    * Use named arg
    
    * Don't forget to camelCase
    
    * I C python doesn't really like enum to int
    
    * Rename Output to AddressWithAmount
    
    * Add more docs

--- sdk/src/client/bindings/python/examples/09_transaction.py
+++ sdk/src/client/bindings/python/examples/09_transaction.py
@@ -1,4 +1,4 @@
-from iota_client import IotaClient, MnemonicSecretManager
+from iota_client import IotaClient, MnemonicSecretManager, AddressWithAmount
 
 # Create an IotaClient instance
 client = IotaClient(nodes = ['https://api.testnet.shimmer.network'])
@@ -6,13 +6,7 @@ client = IotaClient(nodes = ['https://api.testnet.shimmer.network'])
 secret_manager = MnemonicSecretManager("flame fever pig forward exact dash body idea link scrub tennis minute " +
                                        "surge unaware prosper over waste kitten ceiling human knife arch situate civil")
 
-options = {
-    "output": {
-        "address": 'rms1qzpf0tzpf8yqej5zyhjl9k3km7y6j0xjnxxh7m2g3jtj2z5grej67sl6l46',
-        "amount": '1000000',
-    }
-}
-
 # Create and post a block with a transaction
-block = client.build_and_post_block(secret_manager, options)
+block = client.build_and_post_block(secret_manager, output=AddressWithAmount(
+    address='rms1qzpf0tzpf8yqej5zyhjl9k3km7y6j0xjnxxh7m2g3jtj2z5grej67sl6l46', amount=1000000))
 print(f'{block}')
