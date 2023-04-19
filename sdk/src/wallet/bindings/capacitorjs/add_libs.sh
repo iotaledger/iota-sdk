7975cc7a283f77196971a3177d572e23984d94f2
Brord van Wierst <brord@iota.org>
2023-04-19 13:37:19 +0200

    updated maven release and add_libs file

--- sdk/src/wallet/bindings/capacitorjs/add_libs.sh
+++ sdk/src/wallet/bindings/capacitorjs/add_libs.sh
@@ -2,12 +2,15 @@
 set -e
 rm -rf tmp && mkdir tmp && cd tmp
 
-echo Installing Java libraries
-curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-1.0.0-rc.3/jniLibs.zip > iota-wallet-java.zip
+# retrieve the latest version of org.iota.iota-wallet from Maven Central
+latest_version=$(wget -qO- "https://search.maven.org/solrsearch/select?q=g:%22org.iota%22+AND+a:%22iota-wallet%22&core=gav&rows=1&wt=json" | grep -oP '"v":"\K[^"]+' | head -1)
+
+echo Installing Java libraries v$latest_version
+curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-$latest_version/jniLibs.zip > iota-wallet-java.zip
 unzip iota-wallet-java.zip             
 rm -rf ../android/src/main/jniLibs
 cp -rv jniLibs ../android/src/main
-curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-1.0.0-rc.3/iota-wallet-1.0.0-rc.3.jar > iota-wallet.jar
+curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-$latest_version/iota-wallet-$latest_version.jar > iota-wallet.jar
 rm -rf ../android/libs && mkdir -p ../android/libs             
 cp -rv iota-wallet.jar ../android/libs
 
