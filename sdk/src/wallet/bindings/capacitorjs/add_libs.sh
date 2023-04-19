836c5a89bca567115ce7fed4897e527a13a72758
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-04-19 12:50:55 +0200

    chore: update script pointing latest github release (#284)
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- sdk/src/wallet/bindings/capacitorjs/add_libs.sh
+++ sdk/src/wallet/bindings/capacitorjs/add_libs.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# 🛑 temporarily using host https://files.iota.org/firefly/bindings until CI will be done.
 set -e
 rm -rf tmp && mkdir tmp && cd tmp
+
 echo Installing Java libraries
-# curl -SL --progress-bar --fail https://github.com/iotaledger/wallet.rs/releases/download/iota-wallet-java-1.0.0-rc.3-new/iota-wallet-1.0.0-rc.3-android.zip > iota-wallet-java.zip
-curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/jniLibs-1.0.0-alpha.0.zip > iota-wallet-java.zip
+curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-1.0.0-rc.3/jniLibs.zip > iota-wallet-java.zip
 unzip iota-wallet-java.zip             
 rm -rf ../android/src/main/jniLibs
 cp -rv jniLibs ../android/src/main
-curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/iota-wallet-1.0.0-alpha.0.jar > iota-wallet.jar
+curl -SL --progress-bar --fail https://github.com/iotaledger/iota-sdk/releases/download/iota-wallet-java-1.0.0-rc.3/iota-wallet-1.0.0-rc.3.jar > iota-wallet.jar
 rm -rf ../android/libs && mkdir -p ../android/libs             
 cp -rv iota-wallet.jar ../android/libs
+
+# 🛑 temporarily using host https://files.iota.org/firefly/bindings until Swift mobile CI will be done.
 echo Installing Swift libraries
 curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/WalletFramework-1.0.0-alpha.0.zip > iota-wallet-swift.zip
 unzip iota-wallet-swift.zip             
