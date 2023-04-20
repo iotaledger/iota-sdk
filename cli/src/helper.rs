ffc615113021308cd4743e98a675996b64907125
Pawel Iwan <pawel.ivan@iota.org>
2023-04-20 08:16:28 +0200

    style: bring back deleted license

--- cli/src/helper.rs
+++ cli/src/helper.rs
@@ -1,3 +1,6 @@
+// Copyright 2020-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use chrono::{DateTime, NaiveDateTime, Utc};
 use dialoguer::{console::Term, theme::ColorfulTheme, Password, Select};
 use iota_sdk::wallet::Wallet;
