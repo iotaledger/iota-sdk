daa785b200b447d97ba6693fc235b33f3771b826
Alexander Schmidt <alexander.schmidt@iota.org>
2023-04-28 12:51:23 +0200

    add some storage tests

--- sdk/src/wallet/storage/mod.rs
+++ sdk/src/wallet/storage/mod.rs
@@ -38,6 +38,7 @@ impl Storage {
                         Ok(Some(serde_json::from_str(&String::from_utf8_lossy(
                             &chacha::aead_decrypt(key, record.as_bytes())?,
                         ))?))
+                        // Ok(Some(serde_json::from_str(&decrypt_record(&record, key).unwrap())?))
                     } else {
                         Ok(Some(serde_json::from_str(&record)?))
                     }
@@ -56,6 +57,8 @@ impl Storage {
                 key,
                 if let Some(key) = &self.encryption_key {
                     let output = chacha::aead_encrypt(key, record.as_bytes())?;
+                    // let mut output = Vec::new();
+                    // encrypt_record(record.as_bytes(), key, &mut output).unwrap();
                     serde_json::to_string(&output)?
                 } else {
                     record
@@ -88,5 +91,107 @@ impl Storage {
 impl Drop for Storage {
     fn drop(&mut self) {
         log::debug!("drop Storage");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::wallet::storage::adapter::memory::{Memory, STORAGE_ID};
+
+    #[test]
+    fn id() {
+        let storage = Storage {
+            inner: Box::<Memory>::default(),
+            encryption_key: None,
+        };
+
+        assert_eq!(storage.id(), STORAGE_ID);
+    }
+
+    #[tokio::test]
+    async fn get_set_remove() {
+        #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+        struct Record {
+            a: String,
+            b: u32,
+            c: i64,
+        }
+
+        let mut storage = Storage {
+            inner: Box::<Memory>::default(),
+            encryption_key: None,
+        };
+
+        let rec = Record {
+            a: "test".to_string(),
+            b: 42,
+            c: -420,
+        };
+        storage.set("key", rec.clone()).await.unwrap();
+        assert_eq!(Some(rec), storage.get::<Record>("key").await.unwrap());
+
+        storage.remove("key").await.unwrap();
+        assert_eq!(None, storage.get::<Record>("key").await.unwrap());
+    }
+
+    #[cfg(feature = "rand")]
+    #[tokio::test]
+    async fn batch_set() {
+        #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+        struct Record {
+            a: String,
+            b: u32,
+            c: i64,
+        }
+
+        let mut storage = Storage {
+            inner: Box::<Memory>::default(),
+            encryption_key: None,
+        };
+
+        let records = std::iter::repeat_with(|| Record {
+            a: "test".to_string(),
+            b: rand::random(),
+            c: rand::random(),
+        })
+        .enumerate()
+        .map(|(key, record)| (key.to_string(), serde_json::to_string(&record).unwrap()))
+        .take(10)
+        .collect::<HashMap<_, _>>();
+
+        storage.batch_set(records).await.unwrap();
+
+        assert!(storage.get::<Record>("0").await.unwrap().is_some());
+        assert!(storage.get::<Record>("9").await.unwrap().is_some());
+    }
+
+    #[cfg(feature = "rand")]
+    // TODO: uncomment this test when the bug described in Issue #354 has been fixed.
+    // #[tokio::test]
+    #[allow(dead_code)]
+    async fn get_set_encrypted() {
+        #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+        struct Record {
+            a: String,
+            b: u32,
+            c: i64,
+        }
+
+        let encryption_key = crate::types::block::rand::bytes::rand_bytes_array::<32>();
+
+        let mut storage = Storage {
+            inner: Box::<Memory>::default(),
+            encryption_key: Some(encryption_key),
+        };
+
+        let rec = Record {
+            a: "test".to_string(),
+            b: 42,
+            c: -420,
+        };
+        storage.set("key", rec.clone()).await.unwrap();
+
+        assert_eq!(Some(rec), storage.get::<Record>("key").await.unwrap());
     }
 }
