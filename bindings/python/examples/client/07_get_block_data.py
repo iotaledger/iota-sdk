1774697181cec2d10472228aacb24f9dc6755d63
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2023-05-02 10:04:33 +0200

    Use a tip as block id instead of creating a new block (#358)
    * Use a tip as block id instead of creating a new block
    
    * Add Rust example

--- bindings/python/examples/client/07_get_block_data.py
+++ bindings/python/examples/client/07_get_block_data.py
@@ -3,14 +3,14 @@ from iota_sdk import Client
 # Create a Client instance
 client = Client(nodes=['https://api.testnet.shimmer.network'])
 
-# Create and post a block without payload
-blockIdAndBlock = client.build_and_post_block()
-print(f'{blockIdAndBlock}')
+# Fetch a block ID from the node
+block_id = client.get_tips()[0]
+print(f'{block_id}')
 
 # Get the metadata for the block
-metadata = client.get_block_metadata(blockIdAndBlock[0])
+metadata = client.get_block_metadata(block_id)
 print(f'{metadata}')
 
 # Request the block by its id
-block = client.get_block_data(blockIdAndBlock[0])
+block = client.get_block_data(block_id)
 print(f'{block}')
