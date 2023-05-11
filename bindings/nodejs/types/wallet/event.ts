3f1e01734a2f3e8d8f211186cb7ed9372d3ff61f
Thibault Martinez <thibault@iota.org>
2023-05-11 15:46:53 +0200

    Start new WalletEventType enum

--- bindings/nodejs/types/wallet/event.ts
+++ bindings/nodejs/types/wallet/event.ts
@@ -1,28 +1,119 @@
 import type { IOutputResponse, ITransactionPayload } from '@iota/types';
 import type { OutputData } from './output';
 
-/** Wallet event types */
-export type EventType =
-    | '*'
-    | 'ConsolidationRequired'
-    | 'LedgerAddressGeneration'
-    | 'NewOutput'
-    | 'SpentOutput'
-    | 'TransactionInclusion'
-    | 'TransactionProgress';
+/**
+ * All of the wallet event types.
+ */
+enum WalletEventType {
+    ConsolidationRequired = 0,
+    LedgerAddressGeneration = 1,
+    NewOutput = 2,
+    SpentOutput = 3,
+    TransactionInclusion = 4,
+    TransactionProgress = 5
+}
 
-export type NewOutputEvent = {
+abstract class WalletEvent {
+    private type: WalletEventType;
+
+    constructor(type: WalletEventType) {
+        this.type = type;
+    }
+
+    /**
+     * The type of the wallet event.
+     */
+    getType(): WalletEventType {
+        return this.type;
+    }
+}
+
+// SpentOutput = 3,
+// TransactionInclusion = 4,
+// TransactionProgress = 5
+
+class ConsolidationRequiredWalletEvent extends WalletEvent {
+    constructor() {
+        super(WalletEventType.ConsolidationRequired);
+    }
+}
+
+class LedgerAddressGenerationWalletEvent extends WalletEvent {
+    private address: string;
+
+    constructor(address: string) {
+        super(WalletEventType.LedgerAddressGeneration);
+        this.address = address;
+    }
+
+    /**
+    * The address.
+    */
+    getAddress(): string {
+        return this.address;
+    }
+}
+
+class NewOutputWalletEvent extends WalletEvent {
     output: OutputData;
     transaction?: ITransactionPayload;
-    transactionInputs?: IOutputResponse;
-};
+    transactionInputs?: [IOutputResponse];
 
-/** Wallet events */
-export enum WalletEvent {
-    ConsolidationRequired = 'ConsolidationRequired',
-    LedgerAddressGeneration = 'LedgerAddressGeneration',
-    NewOutput = 'NewOutput',
-    SpentOutput = 'SpentOutput',
-    TransactionInclusion = 'TransactionInclusion',
-    TransactionProgress = 'TransactionProgress',
+    constructor(output: OutputData,
+        transaction?: ITransactionPayload,
+        transactionInputs?: [IOutputResponse]) {
+        super(WalletEventType.NewOutput);
+        this.output = output;
+        this.transaction = transaction;
+        this.transactionInputs = transactionInputs;
+    }
+
+    /**
+    * The output.
+    */
+    getOutput(): OutputData {
+        return this.output;
+    }
+
+    /**
+    * The transaction.
+    */
+    getTransaction(): ITransactionPayload | undefined {
+        return this.transaction;
+    }
+
+    /**
+    * The transaction inputs.
+    */
+    getTransactionInputs(): [IOutputResponse] | undefined {
+        return this.transactionInputs;
+    }
 }
+
+class SpentOutputWalletEvent extends WalletEvent {
+    constructor() {
+        super(WalletEventType.SpentOutput);
+    }
+}
+
+class TransactionInclusionWalletEvent extends WalletEvent {
+    constructor() {
+        super(WalletEventType.TransactionInclusion);
+    }
+}
+
+class TransactionProgressWalletEvent extends WalletEvent {
+    constructor() {
+        super(WalletEventType.TransactionProgress);
+    }
+}
+
+export {
+    WalletEvent,
+    ConsolidationRequiredWalletEvent,
+    LedgerAddressGenerationWalletEvent,
+    NewOutputWalletEvent,
+    SpentOutputWalletEvent,
+    TransactionInclusionWalletEvent,
+    TransactionProgressWalletEvent,
+};
