fe54fccc5687812398596764093359d9211f94f6
Pawel Iwan <pawel.ivan@iota.org>
2023-05-12 07:27:25 +0200

    fix: bring back AliasDTO

--- sdk/src/types/block/output/alias_id.rs
+++ sdk/src/types/block/output/alias_id.rs
@@ -30,3 +30,30 @@ impl AliasId {
         Address::Alias(AliasAddress::new(*self)).to_bech32(bech32_hrp)
     }
 }
+
+#[allow(missing_docs)]
+pub mod dto {
+    use alloc::string::ToString;
+
+    use serde::{Deserialize, Serialize};
+
+    use super::*;
+    use crate::types::block::Error;
+
+    #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+    pub struct AliasIdDto(pub String);
+
+    impl From<&AliasId> for AliasIdDto {
+        fn from(value: &AliasId) -> Self {
+            Self(value.to_string())
+        }
+    }
+
+    impl TryFrom<&AliasIdDto> for AliasId {
+        type Error = Error;
+
+        fn try_from(value: &AliasIdDto) -> Result<Self, Self::Error> {
+            value.0.parse::<Self>().map_err(|_| Error::InvalidField("alias id"))
+        }
+    }
+}
