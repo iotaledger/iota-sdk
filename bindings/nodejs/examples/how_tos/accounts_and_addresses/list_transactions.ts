6aa21d3ae0a31ade6645be97ab495b77338f333e
Dr-Electron <dr-electr0n@protonmail.com>
2023-05-15 21:06:59 +0200

    Add "List Transactions" example

---
+++ bindings/nodejs/examples/how_tos/accounts_and_addresses/list_transactions.ts
@@ -0,0 +1,34 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { Wallet, initLogger } from '@iota/sdk';
+
+// Run with command:
+// node ./dist/how_tos/accounts_and_addresses/list_addresses.js
+
+// This example lists all addresses in the account
+async function run() {
+    initLogger();
+    try {
+        const wallet = new Wallet({
+            storagePath: './alice-database',
+        });
+
+        const account = await wallet.getAccount('Alice');
+        await account.sync({ syncIncomingTransactions: true });
+
+        const transactions = await account.transactions();
+        console.log('Sent transactions:');
+        for (const transaction of transactions)
+            console.log(transaction.transactionId);
+
+        const incomingTransactions = await account.incomingTransactions();
+        console.log('Incoming transactions:');
+        for (const transaction of incomingTransactions)
+            console.log(transaction[0]);
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+}
+
+run().then(() => process.exit());
---
+++ sdk/examples/how_tos/accounts_and_addresses/list_transactions.rs
@@ -0,0 +1,39 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! In this example we will list all transaction of an account.
+//!
+//! `cargo run --example list_transactions --release`
+
+use iota_sdk::wallet::{account::SyncOptions, Result, Wallet};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Create the wallet
+    let wallet = Wallet::builder().finish().await?;
+
+    // Get the account
+    let account = wallet.get_account("Alice").await?;
+
+    // Sync account
+    account
+        .sync(Some(SyncOptions {
+            sync_incoming_transactions: true,
+            ..Default::default()
+        }))
+        .await?;
+
+    // Print transaction ids
+    println!("Sent transactions:");
+    for transaction in account.transactions().await? {
+        println!("{}", transaction.transaction_id);
+    }
+
+    // Print received transaction ids
+    println!("Received transactions:");
+    for transaction_id in account.incoming_transactions().await?.keys() {
+        println!("{}", transaction_id);
+    }
+
+    Ok(())
+}
