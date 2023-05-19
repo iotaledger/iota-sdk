18bf066f98c392c2b83234f44e67aa5b21f8bfdc
Alexandcoats <alexandcoats@gmail.com>
2023-05-19 08:37:42 -0400

    LINE PIECE |
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- sdk/src/wallet/wallet/operations/mod.rs
+++ sdk/src/wallet/wallet/operations/mod.rs
@@ -8,7 +8,6 @@ pub(crate) mod client;
 pub(crate) mod get_account;
 #[cfg(feature = "ledger_nano")]
 pub(crate) mod ledger_nano;
-
 pub(crate) mod storage;
 #[cfg(feature = "stronghold")]
 pub(crate) mod stronghold;
