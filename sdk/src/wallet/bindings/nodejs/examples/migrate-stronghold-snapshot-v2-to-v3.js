7d5382e7261fa1306c29dc7bf439c23d9e1dec5d
Thibault Martinez <thibault@iota.org>
2023-05-25 14:23:00 +0200

    Clean up nodejs example

--- sdk/src/wallet/bindings/nodejs/examples/migrate-stronghold-snapshot-v2-to-v3.js
+++ sdk/src/wallet/bindings/nodejs/examples/migrate-stronghold-snapshot-v2-to-v3.js
@@ -1,43 +1,65 @@
-const path = require('path')
+const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-const { AccountManager, CoinType, migrateStrongholdSnapshotV2ToV3 } = require('@iota/wallet');
+const {
+    AccountManager,
+    CoinType,
+    migrateStrongholdSnapshotV2ToV3,
+} = require('@iota/wallet');
 
-var accountManagerOptions = {
-    storagePath: './alice-database',
-    clientOptions: {
-        nodes: [process.env.NODE_URL],
-        localPow: true,
-    },
-    coinType: CoinType.Shimmer,
-    secretManager: {
-        Stronghold: {
-            snapshotPath: "fixtures/v2.stronghold",
-            password: "current_password",
+const v2Path = '../../../../../tests/wallet/fixtures/v2.stronghold';
+const v3Path = './v3.stronghold';
+
+async function run() {
+    var accountManagerOptions = {
+        storagePath: './alice-database',
+        clientOptions: {
+            nodes: [process.env.NODE_URL],
+            localPow: true,
         },
-    },
-};
+        coinType: CoinType.Shimmer,
+        secretManager: {
+            Stronghold: {
+                snapshotPath: v2Path,
+                password: 'current_password',
+            },
+        },
+    };
 
-try {
-    new AccountManager(accountManagerOptions);
-} catch (error) {
-    console.error(error);
+    try {
+        // This should fail with error, migration required.
+        new AccountManager(accountManagerOptions);
+    } catch (error) {
+        console.error(error);
+    }
+
+    migrateStrongholdSnapshotV2ToV3(
+        v2Path,
+        'current_password',
+        'wallet.rs',
+        100,
+        v3Path,
+        'new_password',
+    );
+
+    accountManagerOptions = {
+        storagePath: './alice-database',
+        clientOptions: {
+            nodes: [process.env.NODE_URL],
+            localPow: true,
+        },
+        coinType: CoinType.Shimmer,
+        secretManager: {
+            Stronghold: {
+                snapshotPath: v3Path,
+                password: 'new_password',
+            },
+        },
+    };
+
+    // This shouldn't fail anymore as snapshot has been migrated.
+    const manager = new AccountManager(accountManagerOptions);
+
+    process.exit(0);
 }
 
-migrateStrongholdSnapshotV2ToV3("fixtures/v2.stronghold", "current_password", "fixtures/v3.stronghold", "new_password")
-
-accountManagerOptions = {
-    storagePath: './alice-database',
-    clientOptions: {
-        nodes: [process.env.NODE_URL],
-        localPow: true,
-    },
-    coinType: CoinType.Shimmer,
-    secretManager: {
-        Stronghold: {
-            snapshotPath: "fixtures/v3.stronghold",
-            password: "new_password",
-        },
-    },
-};
-
-const manager = new AccountManager(accountManagerOptions);
+run();
