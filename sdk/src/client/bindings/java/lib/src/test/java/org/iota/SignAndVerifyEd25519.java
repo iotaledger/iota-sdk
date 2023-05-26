e20558a3f3ef0e6f2e9d009801f77bd540c81e4c
Thibault Martinez <thibault@iota.org>
2023-05-26 18:08:48 +0200

    Fix java

--- sdk/src/client/bindings/java/lib/src/test/java/org/iota/SignAndVerifyEd25519.java
+++ sdk/src/client/bindings/java/lib/src/test/java/org/iota/SignAndVerifyEd25519.java
@@ -5,7 +5,6 @@ package org.iota;
 
 import org.iota.types.exceptions.ClientException;
 import org.iota.types.exceptions.InitializeClientException;
-import org.iota.types.Segment;
 import org.iota.types.secret.GenerateAddressesOptions;
 import org.iota.types.secret.Range;
 import org.iota.types.secret.MnemonicSecretManager;
@@ -24,13 +23,8 @@ public class SignAndVerifyEd25519 extends ApiTest {
         // `IOTA` hex encoded
         String message = "0x494f5441";
         // [44, 4218, 0, 0, 0] IOTA coin type, first account, first public address
-        UnsignedByte[] byteArray0 = new UnsignedByte[]{new UnsignedByte((byte) 128), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0), new UnsignedByte((byte) 44)};
-        UnsignedByte[] byteArray1 = new UnsignedByte[]{new UnsignedByte((byte) 128), new UnsignedByte((byte) 0), new UnsignedByte((byte) 16), new UnsignedByte((byte) 123)};
-        UnsignedByte[] byteArray2 = new UnsignedByte[]{new UnsignedByte((byte) 128), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0)};
-        UnsignedByte[] byteArray3 = new UnsignedByte[]{new UnsignedByte((byte) 128), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0)};
-        UnsignedByte[] byteArray4 = new UnsignedByte[]{new UnsignedByte((byte) 128), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0), new UnsignedByte((byte) 0)};
-        Segment[] bip32Chain = new Segment[]{new Segment(true, byteArray0), new Segment(true, byteArray1), new Segment(true, byteArray2), new Segment(true, byteArray3), new Segment(true, byteArray4)};
-        Ed25519Signature signature = client.signEd25519(secretManager, message, bip32Chain);
+        Long[] chain = new Long[]{44l, 4218l, 0l, 0l, 0l};
+        Ed25519Signature signature = client.signEd25519(secretManager, message, chain);
 
         String bech32Address = client.hexPublicKeyToBech32Address(signature.getPublicKey(), "atoi");
         String pubKeyHash = client.bech32ToHex(bech32Address);
