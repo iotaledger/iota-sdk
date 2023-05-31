572b5ab49c19803da0929b5f7702751f449780b9
Alexandcoats <alexandcoats@gmail.com>
2023-05-31 09:58:05 -0400

    Update bindings/core/src/method/account.rs
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- bindings/core/src/method/account.rs
+++ bindings/core/src/method/account.rs
@@ -48,7 +48,7 @@ pub enum AccountMethod {
     #[cfg_attr(docsrs, doc(cfg(feature = "participation")))]
     #[serde(rename_all = "camelCase")]
     DeregisterParticipationEvent { event_id: ParticipationEventId },
-    /// Generate new unused Ed25519 addresses.
+    /// Generate new Ed25519 addresses.
     /// Expected response: [`GeneratedEd25519Addresses`](crate::Response::GeneratedEd25519Addresses)
     GenerateEd25519Addresses {
         amount: u32,
