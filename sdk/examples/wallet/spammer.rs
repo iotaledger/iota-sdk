cb1b582f09cf1c42c50668ec95d26376c4ba6521
/alex/ <alexander.schmidt@iota.org>
2023-06-01 12:56:01 +0200

    Enable only the wallet feature in the spammer
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- sdk/examples/wallet/spammer.rs
+++ sdk/examples/wallet/spammer.rs
@@ -5,7 +5,7 @@
 //!
 //! Rename `.env.example` to `.env` first, then run the command:
 //! ```sh
-//! cargo run --release --all-features --example spammer
+//! cargo run --release --features=wallet --example spammer
 //! ```
 
 use std::{env::var, time::Duration};
