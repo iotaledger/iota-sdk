2f57d15c0501fef0f42336accdf67ee39ee0b663
Alexandcoats <alexandcoats@gmail.com>
2023-06-02 04:26:57 -0400

    Merge `is_alias_transition` over `alias_state_transition` (#419)
    * Merge is_alias_transition over alias_state_transition
    
    * Re-add burn check
    
    * clippy
    
    * fix burn

--- sdk/src/client/api/block_builder/input_selection/sender_issuer.rs
+++ sdk/src/client/api/block_builder/input_selection/sender_issuer.rs
@@ -226,13 +226,15 @@ fn get_required_addresses_for_sender_and_issuer(
     // Addresses in the inputs that will be unlocked in the transaction
     let mut unlocked_addresses = HashSet::new();
     for input_signing_data in selected_inputs {
-        let alias_transition = is_alias_transition(input_signing_data, outputs);
-        let (required_unlock_address, unlocked_alias_or_nft_address) =
-            input_signing_data.output.required_and_unlocked_address(
-                current_time,
-                input_signing_data.output_id(),
-                alias_transition.map(|(alias_transition, _)| alias_transition),
-            )?;
+        let alias_transition = is_alias_transition(
+            &input_signing_data.output,
+            *input_signing_data.output_id(),
+            outputs,
+            None,
+        );
+        let (required_unlock_address, unlocked_alias_or_nft_address) = input_signing_data
+            .output
+            .required_and_unlocked_address(current_time, input_signing_data.output_id(), alias_transition)?;
         unlocked_addresses.insert(required_unlock_address);
         if let Some(unlocked_alias_or_nft_address) = unlocked_alias_or_nft_address {
             unlocked_addresses.insert(unlocked_alias_or_nft_address);
