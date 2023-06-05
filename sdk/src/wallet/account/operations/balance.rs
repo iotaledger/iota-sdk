4a4d65dd18ddaa6fb604f3bdd924c47e66aef929
Thibault Martinez <thibault@iota.org>
2023-06-05 16:06:58 +0200

    Use ?

--- sdk/src/wallet/account/operations/balance.rs
+++ sdk/src/wallet/account/operations/balance.rs
@@ -39,13 +39,12 @@ impl Account {
         let addresses_with_unspent_outputs = addresses
             .into_iter()
             .map(|address| {
-                address.convert().map_err(Error::from).and_then(|address| {
-                    account_details
-                        .addresses_with_unspent_outputs
-                        .iter()
-                        .find(|&a| a.address == address)
-                        .ok_or(Error::AddressNotFoundInAccount(address))
-                })
+                let address = address.convert()?;
+                account_details
+                    .addresses_with_unspent_outputs
+                    .iter()
+                    .find(|&a| a.address == address)
+                    .ok_or(Error::AddressNotFoundInAccount(address))
             })
             .collect::<Result<Vec<&_>>>()?;
 
