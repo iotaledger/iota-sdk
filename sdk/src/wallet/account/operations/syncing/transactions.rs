df3d3c2e422c9deb06ec7036c6e8d15dd12612e0
Thibault Martinez <thibault@iota.org>
2023-06-08 13:27:20 +0200

    Clean up API response/dto

--- sdk/src/wallet/account/operations/syncing/transactions.rs
+++ sdk/src/wallet/account/operations/syncing/transactions.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 
 use crate::{
     types::{
-        api::core::dto::LedgerInclusionStateDto,
+        api::core::response::LedgerInclusionState,
         block::{input::Input, output::OutputId, payload::transaction::TransactionEssence, BlockId},
     },
     utils::unix_timestamp_now,
@@ -102,7 +102,7 @@ impl Account {
                     Ok(metadata) => {
                         if let Some(inclusion_state) = metadata.ledger_inclusion_state {
                             match inclusion_state {
-                                LedgerInclusionStateDto::Included => {
+                                LedgerInclusionState::Included => {
                                     log::debug!(
                                         "[SYNC] confirmed transaction {transaction_id} in block {}",
                                         metadata.block_id
@@ -116,7 +116,7 @@ impl Account {
                                         &mut spent_output_ids,
                                     );
                                 }
-                                LedgerInclusionStateDto::Conflicting => {
+                                LedgerInclusionState::Conflicting => {
                                     // try to get the included block, because maybe only this attachment is
                                     // conflicting because it got confirmed in another block
                                     if let Ok(included_block) =
@@ -142,7 +142,7 @@ impl Account {
                                         );
                                     }
                                 }
-                                LedgerInclusionStateDto::NoTransaction => {
+                                LedgerInclusionState::NoTransaction => {
                                     unreachable!(
                                         "We should only get the metadata for blocks with a transaction payload"
                                     )
