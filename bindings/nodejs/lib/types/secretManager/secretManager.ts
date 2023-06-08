103de1ba18cc36c9e42bbd0e4cd1c35a6e004932
Brord van Wierst <brord@iota.org>
2023-06-08 11:56:48 +0200

    fmt and rename

--- bindings/nodejs/lib/types/secretManager/secretManager.ts
+++ bindings/nodejs/lib/types/secretManager/secretManager.ts
@@ -1,7 +1,7 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { HexEncodedString } from "../utils";
+import { HexEncodedString } from '../utils';
 
 /** Secret manager that uses a Ledger Nano hardware wallet or Speculos simulator. */
 export interface LedgerNanoSecretManager {
