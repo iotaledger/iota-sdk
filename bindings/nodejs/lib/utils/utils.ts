5d640d2006c504acbb7068faeaabdab6e92cd252
Brord van Wierst <brord@iota.org>
2023-06-09 13:14:18 +0200

    hex 2.0

--- bindings/nodejs/lib/utils/utils.ts
+++ bindings/nodejs/lib/utils/utils.ts
@@ -185,7 +185,7 @@ export class Utils {
     /**
      * Compute the hash of a transaction essence.
      */
-    static hashTransactionEssence(essence: TransactionEssence): string {
+    static hashTransactionEssence(essence: TransactionEssence): HexEncodedString {
         return callUtilsMethod({
             name: 'hashTransactionEssence',
             data: {
