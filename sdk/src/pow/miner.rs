b147d8149e954b5a0ab8f53fa1d50e40e84cc5f2
Pawel Iwan <pawel.ivan@iota.org>
2023-06-09 11:36:47 +0200

    fix: clippy

--- sdk/src/pow/miner.rs
+++ sdk/src/pow/miner.rs
@@ -80,7 +80,7 @@ impl MinerBuilder {
     /// Builds the [`Miner`].
     pub fn finish(self) -> Miner {
         Miner {
-            num_workers: self.num_workers.unwrap_or(num_cpus::get()),
+            num_workers: self.num_workers.unwrap_or_else(num_cpus::get),
             cancel: self.cancel.unwrap_or_else(MinerCancel::new),
         }
     }
