eaadd616c8e92030af522a5109cee2b138661adc
Dr-Electron <dr-electr0n@protonmail.com>
2023-06-12 22:07:51 +0200

    Forgot one

--- bindings/nodejs/examples/how_tos/native_tokens/send.ts
+++ bindings/nodejs/examples/how_tos/native_tokens/send.ts
@@ -1,9 +1,7 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-    SendNativeTokensParams,
-} from '@iota/sdk';
+import { SendNativeTokensParams } from '@iota/sdk';
 
 import { getUnlockedWallet } from '../../wallet/common';
 
