fda0232f8d55657eb854135c5fa7d8046701f8b6
Pawel Iwan <pawel.ivan@iota.org>
2023-06-15 08:27:35 +0200

    fix: change default permissions for mnemonic file

--- cli/src/helper.rs
+++ cli/src/helper.rs
@@ -195,7 +195,12 @@ pub async fn import_mnemonic(path: &str) -> Result<String, Error> {
 }
 
 async fn write_mnemonic_to_file(path: &str, mnemonic: &str) -> Result<(), Error> {
-    let mut file = OpenOptions::new().create(true).append(true).open(path).await?;
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .mode(0o600)
+        .open(path)
+        .await?;
     file.write_all(format!("{mnemonic}\n").as_bytes()).await?;
 
     Ok(())
