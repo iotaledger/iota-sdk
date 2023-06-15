348b611da3b560a169d8dd72c8cde1c6acf2daaa
Alex Coats <alexandcoats@gmail.com>
2023-06-15 08:10:26 -0400

    one more

--- bindings/python/iota_sdk/types/signature.py
+++ bindings/python/iota_sdk/types/signature.py
@@ -6,11 +6,6 @@ from iota_sdk.types.common import HexStr
 class Ed25519Signature():
     def __init__(self, public_key: HexStr, signature: HexStr):
         """Initialize an Ed25519Signature"""
-        
+
         self.public_key = public_key
         self.signature = signature
-
-    def as_dict(self):
-        config = {k: v for k, v in self.__dict__.items() if v != None}
-
-        return config
