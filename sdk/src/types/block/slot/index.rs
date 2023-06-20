3cdbb9c490fc00236261905e01ec4ecc5590f733
Thibault Martinez <thibault@iota.org>
2023-06-20 20:04:29 +0200

    Add deref

--- sdk/src/types/block/slot/index.rs
+++ sdk/src/types/block/slot/index.rs
@@ -1,10 +1,12 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use derive_more::{Deref, From};
+
 /// Timeline is divided into slots, and each slot has a corresponding slot index.
 /// To calculate the slot index of a timestamp, `genesisTimestamp` and the duration of a slot are needed.
 /// The slot index of timestamp `ts` is `(ts - genesisTimestamp)/duration + 1`.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, derive_more::From, packable::Packable)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, From, Deref, packable::Packable)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct SlotIndex(u64);
 
