d2ff171ef082a672415487e513d7ba928a141894
Thibault Martinez <thibault@iota.org>
2023-06-20 17:43:34 +0200

    Add SlotCommitmentId

--- sdk/src/types/block/slot/mod.rs
+++ sdk/src/types/block/slot/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod commitment_id;
 mod index;
 
-pub use self::index::SlotIndex;
+pub use self::{commitment_id::SlotCommitmentId, index::SlotIndex};
