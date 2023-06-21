c4962a390500e9822b042699fc9760adc1660476
Thibault Martinez <thibault@iota.org>
2023-06-21 09:31:50 +0200

    Remove it

--- sdk/src/client/api/block_builder/input_selection/helpers.rs
+++ sdk/src/client/api/block_builder/input_selection/helpers.rs
@@ -13,25 +13,6 @@ use crate::{
     },
 };
 
-// Dedup inputs by output id, because other data could be different, even if it's the same output
-// TODO remove ?
-// pub(crate) fn dedup_inputs(
-//     mandatory_inputs: &mut Vec<InputSigningData>,
-//     additional_inputs: &mut Vec<InputSigningData>,
-// ) { // Sorting inputs by OutputId so duplicates can be safely removed. mandatory_inputs.sort_by_key(|input|
-//   *input.output_metadata.output_id()); mandatory_inputs.dedup_by_key(|input| *input.output_metadata.output_id());
-//   additional_inputs.sort_by_key(|input| *input.output_metadata.output_id()); additional_inputs.dedup_by_key(|input|
-//   *input.output_metadata.output_id());
-
-//     // Remove additional inputs that are already mandatory.
-//     // TODO: could be done more efficiently with itertools unique?
-//     additional_inputs.retain(|input| {
-//         !mandatory_inputs
-//             .iter()
-//             .any(|mandatory_input| input.output_metadata.output_id() == mandatory_input.output_metadata.output_id())
-//     });
-// }
-
 /// Computes the minimum storage deposit amount that a basic output needs to have with an [AddressUnlockCondition] and
 /// optional [NativeTokens].
 pub fn minimum_storage_deposit_basic_output(
