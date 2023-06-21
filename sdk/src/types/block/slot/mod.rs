a46f756054609853c93a747d1e939cfd843d3829
Thibault Martinez <thibault@iota.org>
2023-06-21 12:13:20 +0200

    Add slot related types (#642)
    * Add SlotIndex
    
    * Slot module
    
    * Add SlotCommitmentId
    
    * Add SlotCommitment::id
    
    * Add RootsId
    
    * Add SlotCommitment
    
    * Add deref
    
    * Added getters
    
    * Use internal index
    
    * Update sdk/src/types/block/slot/commitment.rs
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Update sdk/src/types/block/slot/commitment.rs
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

---
+++ sdk/src/types/block/slot/mod.rs
@@ -0,0 +1,9 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod commitment;
+mod commitment_id;
+mod index;
+mod roots_id;
+
+pub use self::{commitment::SlotCommitment, commitment_id::SlotCommitmentId, index::SlotIndex, roots_id::RootsId};
