cc536449411c8cddba9cc6d78cf8e200e0b99664
Alexander Schmidt <alexander.schmidt@iota.org>
2023-06-22 10:19:38 +0200

    nit

--- sdk/examples/how_tos/native_tokens/decrease_supply.rs
+++ sdk/examples/how_tos/native_tokens/decrease_supply.rs
@@ -64,7 +64,7 @@ async fn main() -> Result<()> {
         .await?;
 
     println!(
-        "Block included: {}/block/{}",
+        "Transaction included: {}/block/{}",
         std::env::var("EXPLORER_URL").unwrap(),
         block_id
     );
