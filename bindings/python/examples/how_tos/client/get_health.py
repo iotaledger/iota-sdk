03a57efafcc05b37c94e64f6bd258a1e3ddbb1c4
Dr-Electron <dr-electr0n@protonmail.com>
2023-06-23 09:23:14 +0200

    Update bindings/python/examples/how_tos/client/get_health.py
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- bindings/python/examples/how_tos/client/get_health.py
+++ bindings/python/examples/how_tos/client/get_health.py
@@ -10,6 +10,6 @@ node_url = os.environ.get('NODE_URL', 'https://api.testnet.shimmer.network')
 # Create a Client instance
 client = Client(nodes=[node_url])
 
-# Get the node info
+# Get the node health
 is_healthy = client.get_health(node_url)
 print(f'Healthy: {is_healthy}')
