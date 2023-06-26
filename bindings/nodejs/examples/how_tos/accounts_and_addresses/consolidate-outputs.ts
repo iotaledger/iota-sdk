1beee4c930dbafe663ef2b412481f49da4ea746e
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-06-26 13:19:14 +0200

    Add env variables set

--- bindings/nodejs/examples/how_tos/accounts_and_addresses/consolidate-outputs.ts
+++ bindings/nodejs/examples/how_tos/accounts_and_addresses/consolidate-outputs.ts
@@ -14,10 +14,20 @@ require('dotenv').config({ path: '.env' });
 async function run() {
     initLogger();
     try {
+        if (!process.env.WALLET_DB_PATH) {
+            throw new Error(
+                '.env WALLET_DB_PATH is undefined, see .env.example',
+            );
+        }
+
         if (!process.env.STRONGHOLD_PASSWORD) {
             throw new Error(
                 '.env STRONGHOLD_PASSWORD is undefined, see .env.example',
             );
+        }
+
+        if (!process.env.EXPLORER_URL) {
+            throw new Error('.env EXPLORER_URL is undefined, see .env.example');
         }
 
         const wallet = new Wallet({
