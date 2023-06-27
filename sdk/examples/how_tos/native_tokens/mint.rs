c0d0b09e55d798983abc64f0d1ff3794451ad03c
Alexander Schmidt <alexander.schmidt@iota.org>
2023-06-27 09:59:19 +0200

    fmt

--- sdk/examples/how_tos/native_tokens/mint.rs
+++ sdk/examples/how_tos/native_tokens/mint.rs
@@ -56,9 +56,7 @@ async fn main() -> Result<()> {
 
     // Mint some more native tokens
     let mint_amount = U256::from(MINT_AMOUNT);
-    let transaction = account
-        .mint_native_token(token_id, mint_amount, None)
-        .await?;
+    let transaction = account.mint_native_token(token_id, mint_amount, None).await?;
     println!("Transaction sent: {}", transaction.transaction.transaction_id);
 
     let block_id = account
