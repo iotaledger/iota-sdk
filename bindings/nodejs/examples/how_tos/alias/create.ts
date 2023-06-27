49c8c255246b4827c2d5c3225d54ef2669b1f25d
Kumar Anirudha <mail@anirudha.dev>
2023-06-28 04:16:10 +0530

    added create and destroy alias

---
+++ bindings/nodejs/examples/how_tos/alias/create.ts
@@ -0,0 +1,61 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { Utils, Wallet, initLogger } from '@iota/sdk';
+
+// This example uses secrets in environment variables for simplicity which should not be done in production.
+require('dotenv').config({ path: '.env' });
+
+// Run with command:
+// yarn run-example ./how_tos/alias/create.ts
+
+// In this example we create alias.
+async function run() {
+    initLogger();
+    if (!process.env.FAUCET_URL) {
+        throw new Error('.env FAUCET_URL is undefined, see .env.example');
+    }
+    try {
+        // Create the wallet
+        const wallet = new Wallet({
+            storagePath: process.env.WALLET_DB_PATH,
+        });
+
+        // Get the account we generated with `01-create-wallet`
+        const account = await wallet.getAccount('Alice');
+
+        // May want to ensure the account is synced before sending a transaction.
+        let balance = await account.sync();
+
+        if (balance.aliases.length == 0) {
+            throw new Error(`No Alias available in account 'Alice'`);
+        }
+
+        console.log(`Aliases BEFORE:\n`, balance.aliases);
+
+        console.log('Sending the create-alias transaction...');
+
+        // Create an alias
+        const transaction = await account
+            .prepareCreateAliasOutput()
+            .then((prepared) => prepared.send());
+
+        console.log(`Transaction sent: ${transaction.transactionId}`);
+
+        // Wait for transaction to get included
+        const blockId = await account.retryTransactionUntilIncluded(
+            transaction.transactionId,
+        );
+        console.log(
+            `Transaction included: ${process.env.EXPLORER_URL}/block/${blockId}`,
+        );
+
+        balance = await account.sync();
+        console.log(`Aliases AFTER:\n`, balance.aliases);
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+    process.exit(0);
+}
+
+run();
---
+++ bindings/python/examples/how_tos/alias/create.py
@@ -0,0 +1,23 @@
+from iota_sdk import Wallet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# In this example we will create an alias ouput
+
+wallet = Wallet('./alice-database')
+
+account = wallet.get_account('Alice')
+
+# Sync account with the node
+account.sync()
+
+if 'STRONGHOLD_PASSWORD' not in os.environ:
+    raise Exception(".env STRONGHOLD_PASSWORD is undefined, see .env.example")    
+
+wallet.set_stronghold_password(os.environ["STRONGHOLD_PASSWORD"])
+
+# Send transaction.
+transaction = account.prepare_create_alias_output(None, None).send()
+print(f'Block sent: {os.environ["EXPLORER_URL"]}/block/{transaction["blockId"]}')
