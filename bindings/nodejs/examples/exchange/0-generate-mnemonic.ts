63361330ea6ac4e62a6253da024b3a36f26f5f9d
Thibault Martinez <thibault@iota.org>
2023-07-04 18:25:22 +0200

    Clean 1-create-account.ts

--- bindings/nodejs/examples/exchange/0-generate-mnemonic.ts
+++ bindings/nodejs/examples/exchange/0-generate-mnemonic.ts
@@ -9,8 +9,8 @@ import { Utils } from '@iota/sdk';
 // This example generates a new random mnemonic.
 async function run() {
     try {
-        console.log('Generated mnemonic:', Utils.generateMnemonic());
-        // Set generated mnemonic as env variable for MNEMONIC so it can be used in 1-create-account.js
+        console.log('Mnemonic:', Utils.generateMnemonic());
+        // Set generated mnemonic as env variable for MNEMONIC so it can be used in the next examples.
     } catch (error) {
         console.log('Error: ', error);
     }
--- bindings/nodejs/examples/exchange/1-create-account.ts
+++ bindings/nodejs/examples/exchange/1-create-account.ts
@@ -1,39 +1,58 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { AccountManager, CoinType } from '@iota/sdk';
+import { Wallet, WalletOptions, CoinType } from '@iota/sdk';
 
 // This example uses secrets in environment variables for simplicity which should not be done in production.
 require('dotenv').config({ path: '.env' });
 
 // Run with command:
-// yarn run-example ./how_tos/accounts_and_addresses/check-balance.ts
+// yarn run-example ./exchange/1-create-account.ts 
 
 // This example creates a new database and account.
 async function run() {
     try {
-        const accountManagerOptions = {
-            storagePath: './alice-database',
+        if (!process.env.NODE_URL) {
+            throw new Error('.env NODE_URL is undefined, see .env.example');
+        }
+        if (!process.env.STRONGHOLD_PASSWORD) {
+            throw new Error(
+                '.env STRONGHOLD_PASSWORD is undefined, see .env.example',
+            );
+        }
+        if (!process.env.STRONGHOLD_SNAPSHOT_PATH) {
+            throw new Error(
+                '.env STRONGHOLD_SNAPSHOT_PATH is undefined, see .env.example',
+            );
+        }
+        if (!process.env.MNEMONIC) {
+            throw new Error(
+                '.env MNEMONIC is undefined, see .env.example',
+            );
+        }
+
+        const walletOptions: WalletOptions = {
+            storagePath: process.env.WALLET_DB_PATH,
             clientOptions: {
-                nodes: ['https://api.testnet.shimmer.network'],
+                nodes: [process.env.NODE_URL],
             },
             // CoinType.IOTA can be used to access Shimmer staking rewards, but it's
             // recommended to use the Shimmer coin type to be compatible with other wallets.
             coinType: CoinType.Shimmer,
             secretManager: {
-                Stronghold: {
-                    snapshotPath: `./wallet.stronghold`,
+                stronghold: {
+                    snapshotPath: process.env.STRONGHOLD_SNAPSHOT_PATH,
                     password: `${process.env.STRONGHOLD_PASSWORD}`,
                 },
             },
         };
 
-        const manager = new AccountManager(accountManagerOptions);
+        const wallet = new Wallet(walletOptions);
 
-        // Mnemonic only needs to be set the first time
-        await manager.storeMnemonic(process.env.MNEMONIC);
+        // Mnemonic only needs to be set the first time.
+        await wallet.storeMnemonic(process.env.MNEMONIC);
 
-        const account = await manager.createAccount({
+        const account = await wallet.createAccount({
             alias: 'Alice'
         });
         console.log('Account created:', account);
