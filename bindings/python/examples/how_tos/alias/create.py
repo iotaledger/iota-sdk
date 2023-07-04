9567d60cbca814446d455a1dc5ca5b42b4a3a014
Ani <5357586+anistark@users.noreply.github.com>
2023-07-04 22:48:58 +0530

    Update bindings/python/examples/how_tos/alias/create.py
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- bindings/python/examples/how_tos/alias/create.py
+++ bindings/python/examples/how_tos/alias/create.py
@@ -20,4 +20,4 @@ wallet.set_stronghold_password(os.environ["STRONGHOLD_PASSWORD"])
 
 # Send transaction.
 transaction = account.prepare_create_alias_output(None, None).send()
-print(f'Block sent: {os.environ["EXPLORER_URL"]}/block/{transaction["blockId"]}')
+print(f'Block sent: {os.environ["EXPLORER_URL"]}/block/{transaction.blockId}')
