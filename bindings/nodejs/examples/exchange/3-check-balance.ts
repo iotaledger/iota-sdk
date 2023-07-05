5449cc844f39c60ce7cc98e4015901e56b53df5f
Thibault Martinez <thibault@iota.org>
2023-07-05 13:58:19 +0200

    Clean 3-check-balance.ts

--- bindings/nodejs/examples/exchange/3-check-balance.ts
+++ bindings/nodejs/examples/exchange/3-check-balance.ts
@@ -1,31 +1,31 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { AccountManager } from '@iota/sdk';
+import { Wallet } from '@iota/sdk';
 
 // This example uses secrets in environment variables for simplicity which should not be done in production.
 require('dotenv').config({ path: '.env' });
 
 // Run with command:
-// yarn run-example ./how_tos/accounts_and_addresses/check-balance.ts
+// yarn run-example ./exchange/3-check-balance.ts
 
 // This example gets the balance of an account.
 async function run() {
     try {
-        const manager = new AccountManager({
-            storagePath: './alice-database',
+        const wallet = new Wallet({
+            storagePath: process.env.WALLET_DB_PATH,
         });
 
-        const account = await manager.getAccount('Alice');
-        const addressObject = await account.addresses();
-        console.log('Addresses before:', addressObject);
+        const account = await wallet.getAccount('Alice');
+        const addresses = await account.addresses();
 
-        // syncOnlyMostBasicOutputs if not interested in outputs that are timelocked, 
-        // have a storage deposit return, expiration or are nft/alias/foundry outputs
-        const synced = await account.sync({ syncOnlyMostBasicOutputs: true });
-        console.log('Syncing... - ', synced);
+        console.log('Addresses:', addresses);
 
-        console.log('Available balance', await account.getBalance());
+        // Set syncOnlyMostBasicOutputs to true if not interested in outputs that are timelocked, 
+        // have a storage deposit return, expiration or are nft/alias/foundry outputs.
+        const balance = await account.sync({ syncOnlyMostBasicOutputs: true });
+
+        console.log('Balance', balance);
 
         // Use the Faucet to send testnet tokens to your address:
         console.log("Fill your address with the Faucet: https://faucet.testnet.shimmer.network/")
