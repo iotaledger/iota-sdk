1b63eeb23dda8ca7a803fc595e7a5d48022f5ab8
Thibault Martinez <thibault@iota.org>
2023-07-05 17:04:10 +0200

    Update exchange examples (#723)
    * Update exchange examples
    
    * Clean 1-create-account.ts
    
    * Clean 2-generate-address.ts
    
    * Clean 3-check-balance.ts
    
    * Clean 4-listen-events.ts
    
    * Clean 5-send-amount.ts
    
    * Fmt
    
    * Nit
    
    * Nit
    
    * Nits
    
    * Nit
    
    * Last nits
    
    * Fmt
    
    * console.error(error);
    
    * account.setDefaultSyncOptions
    
    * Typo
    
    * Sync before send
    
    * Use EXPLORER_URL

---
+++ bindings/nodejs/examples/exchange/2-generate-address.ts
@@ -0,0 +1,44 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { Wallet } from '@iota/sdk';
+
+// This example uses secrets in environment variables for simplicity which should not be done in production.
+require('dotenv').config({ path: '.env' });
+
+// Run with command:
+// yarn run-example ./exchange/2-generate-address.ts
+
+// This example generates an address for an account.
+async function run() {
+    try {
+        if (!process.env.WALLET_DB_PATH) {
+            throw new Error(
+                '.env WALLET_DB_PATH is undefined, see .env.example',
+            );
+        }
+        if (!process.env.STRONGHOLD_PASSWORD) {
+            throw new Error(
+                '.env STRONGHOLD_PASSWORD is undefined, see .env.example',
+            );
+        }
+
+        const wallet = new Wallet({
+            storagePath: process.env.WALLET_DB_PATH,
+        });
+
+        await wallet.setStrongholdPassword(
+            `${process.env.STRONGHOLD_PASSWORD}`,
+        );
+
+        const account = await wallet.getAccount('Alice');
+
+        const address = (await account.generateEd25519Addresses(1))[0];
+
+        console.log('Address:', address);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+run().then(() => process.exit());
