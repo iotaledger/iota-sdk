4818100afdff81606f54acdf7061a1c9b0cbdc77
Thibault Martinez <thibault@iota.org>
2023-07-05 15:33:19 +0200

    Nit

--- bindings/nodejs/examples/exchange/1-create-account.ts
+++ bindings/nodejs/examples/exchange/1-create-account.ts
@@ -12,35 +12,34 @@ require('dotenv').config({ path: '.env' });
 // This example creates a new database and account.
 async function run() {
     try {
+        if (!process.env.WALLET_DB_PATH) {
+            throw new Error(
+                '.env WALLET_DB_PATH is undefined, see .env.example',
+            );
+        }
         if (!process.env.NODE_URL) {
             throw new Error('.env NODE_URL is undefined, see .env.example');
-        }
-        if (!process.env.STRONGHOLD_PASSWORD) {
-            throw new Error(
-                '.env STRONGHOLD_PASSWORD is undefined, see .env.example',
-            );
         }
         if (!process.env.STRONGHOLD_SNAPSHOT_PATH) {
             throw new Error(
                 '.env STRONGHOLD_SNAPSHOT_PATH is undefined, see .env.example',
             );
         }
+        if (!process.env.STRONGHOLD_PASSWORD) {
+            throw new Error(
+                '.env STRONGHOLD_PASSWORD is undefined, see .env.example',
+            );
+        }
         if (!process.env.MNEMONIC) {
             throw new Error('.env MNEMONIC is undefined, see .env.example');
         }
-        if (!process.env.WALLET_DB_PATH) {
-            throw new Error(
-                '.env WALLET_DB_PATH is undefined, see .env.example',
-            );
-        }
+
 
         const walletOptions: WalletOptions = {
             storagePath: process.env.WALLET_DB_PATH,
             clientOptions: {
                 nodes: [process.env.NODE_URL],
             },
-            // CoinType.IOTA can be used to access Shimmer staking rewards, but it's
-            // recommended to use the Shimmer coin type to be compatible with other wallets.
             coinType: CoinType.Shimmer,
             secretManager: {
                 stronghold: {
@@ -58,7 +57,8 @@ async function run() {
         const account = await wallet.createAccount({
             alias: 'Alice',
         });
-        console.log('Account created:', account);
+
+        console.log(account);
     } catch (error) {
         console.log('Error: ', error);
     }
