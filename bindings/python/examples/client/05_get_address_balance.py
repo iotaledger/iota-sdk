b985c2b57f9b4f6886dfeaf0958770a2c1c37c38
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2023-07-07 13:47:52 +0200

    Python output class (#726)
    * Python output class
    
    * Review suggestion
    
    * Rename var
    
    * Update comments, add OutputType enum

--- bindings/python/examples/client/05_get_address_balance.py
+++ bindings/python/examples/client/05_get_address_balance.py
@@ -19,7 +19,7 @@ query_parameters = NodeIndexerAPI.QueryParameters(
 
 # Get output ids of basic outputs that can be controlled by this address without further unlock constraints
 output_ids_response = client.basic_output_ids(query_parameters)
-print(f'{output_ids_response}')
+print(f'{output_ids_response.items}')
 
 # Get the outputs by their id
 outputs = client.get_outputs(output_ids_response.items)
@@ -29,11 +29,11 @@ print(f'{outputs}')
 # Calculate the total amount and native tokens
 total_amount = 0
 native_tokens = []
-for output_response in outputs:
-    output = output_response['output']
-    total_amount += int(output['amount'])
-    if 'nativeTokens' in output:
-        native_tokens.append(output['nativeTokens'])
+for output_with_metadata in outputs:
+    output = output_with_metadata.output
+    total_amount += int(output.amount)
+    if output.nativeTokens:
+        native_tokens.append(output.nativeTokens)
 
 print(
     f'Outputs controlled by {address} have {total_amount} glow and native tokens: {native_tokens}')
