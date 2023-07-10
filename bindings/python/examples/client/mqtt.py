76de93c49a3437bc59813e017afcc9ad4dfe3d15
Thoralf Müller <thoralf.mue@gmail.com>
2023-07-10 22:24:55 +0200

    More efficient way of waiting

--- bindings/python/examples/client/mqtt.py
+++ bindings/python/examples/client/mqtt.py
@@ -1,6 +1,7 @@
 from iota_sdk import Client, init_logger
 from dotenv import load_dotenv
 import os
+import threading
 import json
 
 load_dotenv()
@@ -12,19 +13,22 @@ client = Client(nodes=[node_url])
 
 received_events = 0
 
+received_10_events = threading.Event()
+
 
 def callback(event):
     event_dict = json.loads(event)
     print(event_dict)
     global received_events
     received_events += 1
+    if received_events > 10:
+        received_10_events.set()
 
 
 # Topics can be found here https://studio.asyncapi.com/?url=https://raw.githubusercontent.com/iotaledger/tips/main/tips/TIP-0028/event-api.yml
 client.listen_mqtt(["blocks"], callback)
 
 # Exit after 10 received events
-while True:
-    if received_events > 10:
-        client.clear_mqtt_listeners(["blocks"])
-        exit()
+while not received_10_events.wait():
+    client.clear_mqtt_listeners(["blocks"])
+    exit()
