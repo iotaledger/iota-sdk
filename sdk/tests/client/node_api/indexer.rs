549d0ea0acb0c94cf9f515c95c12bf47a44743e3
Thibault Martinez <thibault@iota.org>
2023-07-12 11:01:04 +0200

    Fix test

--- sdk/tests/client/node_api/indexer.rs
+++ sdk/tests/client/node_api/indexer.rs
@@ -17,14 +17,13 @@ use crate::client::common::create_client_and_secret_manager_with_funds;
 async fn get_alias_output_id_test() -> Result<()> {
     let (client, secret_manager) = create_client_and_secret_manager_with_funds(None).await?;
     let protocol_parameters = client.get_protocol_parameters().await?;
-    let alias_id = AliasId::null();
 
     let address = secret_manager
         .generate_ed25519_addresses(GetAddressesOptions::from_client(&client).await?.with_range(0..3))
         .await?[0];
 
     let alias_output =
-        AliasOutputBuilder::new_with_minimum_storage_deposit(*protocol_parameters.rent_structure(), alias_id)
+        AliasOutputBuilder::new_with_minimum_storage_deposit(*protocol_parameters.rent_structure(), AliasId::null())
             .with_state_metadata([1, 2, 3])
             .add_unlock_condition(StateControllerAddressUnlockCondition::new(address))
             .add_unlock_condition(GovernorAddressUnlockCondition::new(address))
@@ -40,7 +39,7 @@ async fn get_alias_output_id_test() -> Result<()> {
     client.retry_until_included(&block.id(), None, None).await?;
 
     let output_id_0 = get_alias_output_id(block.payload().unwrap())?;
-    let output_id_1 = client.alias_output_id(alias_id).await?;
+    let output_id_1 = client.alias_output_id(AliasId::from(&output_id_0)).await?;
 
     assert_eq!(output_id_0, output_id_1);
 
