5fc249f40ed9b597c23ea01784a41fd4a546f572
Thibault Martinez <thibault@iota.org>
2023-07-17 13:38:33 +0200

    Fmt

--- bindings/nodejs/examples/client/10-mqtt.ts
+++ bindings/nodejs/examples/client/10-mqtt.ts
@@ -1,11 +1,7 @@
 // Copyright 2021-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-    Block,
-    Client,
-    initLogger,
-} from '@iota/sdk';
+import { Block, Client, initLogger } from '@iota/sdk';
 import { plainToInstance } from 'class-transformer';
 
 require('dotenv').config({ path: '.env' });
