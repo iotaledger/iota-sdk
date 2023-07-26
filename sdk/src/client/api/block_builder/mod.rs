ade7e66871fe779f06e45ef645482c9ad4265ce3
Thibault Martinez <thibault@iota.org>
2023-07-26 09:42:21 +0200

    Fix compilation

--- sdk/src/client/api/block_builder/mod.rs
+++ sdk/src/client/api/block_builder/mod.rs
@@ -2,7 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod input_selection;
-pub mod pow;
 pub mod transaction;
 
-pub use transaction::verify_semantic;
+pub use self::transaction::verify_semantic;
+use crate::{
+    client::{ClientInner, Result},
+    types::block::{parent::StrongParents, payload::Payload, Block, BlockBuilder},
+};
+
+impl ClientInner {
+    /// Finishes the block with local PoW if needed.
+    /// Without local PoW, it will finish the block with a 0 nonce.
+    pub async fn finish_block_builder(
+        &self,
+        strong_parents: Option<StrongParents>,
+        payload: Option<Payload>,
+    ) -> Result<Block> {
+        let strong_parents = match strong_parents {
+            Some(strong_parents) => strong_parents,
+            None => StrongParents::from_vec(self.get_tips().await?)?,
+        };
+
+        Ok(BlockBuilder::new(strong_parents).with_payload(payload).finish()?)
+    }
+}
