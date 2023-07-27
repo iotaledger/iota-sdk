b7ce1d1b9406350bfa76b85c2631895b091981a8
Thibault Martinez <thibault@iota.org>
2023-07-27 12:42:48 +0200

    Fix compilation

--- sdk/src/types/block/output/feature/staking.rs
+++ sdk/src/types/block/output/feature/staking.rs
@@ -50,8 +50,7 @@ impl StakingFeature {
     }
 }
 
-#[allow(missing_docs)]
-pub mod dto {
+pub(crate) mod dto {
     use serde::{Deserialize, Serialize};
 
     #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
