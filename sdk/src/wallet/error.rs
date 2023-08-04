cc434a0bbe4405e8080078ed6815e4bdfcefdeae
sparnerkar <s.parnerkar@etogruppe.com>
2023-08-04 15:12:58 +0200

    added from impl

--- sdk/src/wallet/error.rs
+++ sdk/src/wallet/error.rs
@@ -189,3 +189,10 @@ impl From<rocksdb::Error> for Error {
         Self::Storage(error.to_string())
     }
 }
+
+#[cfg(feature = "jammdb")]
+impl From<jammdb::Error> for Error {
+    fn from(error: jammdb::Error) -> Self {
+        Self::Storage(error.to_string())
+    }
+}
