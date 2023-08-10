1bd035c61188c30ca3f69539d668441c90ec760c
Brord van Wierst <brord@iota.org>
2023-08-10 23:19:33 +0200

    fmt...

--- bindings/nodejs/examples/client/10-mqtt.ts
+++ bindings/nodejs/examples/client/10-mqtt.ts
@@ -1,12 +1,7 @@
 // Copyright 2021-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-    BasicBlock,
-    Client,
-    initLogger,
-    parseBlock,
-} from '@iota/sdk';
+import { BasicBlock, Client, initLogger, parseBlock } from '@iota/sdk';
 
 require('dotenv').config({ path: '.env' });
 
