11cbed27b4952323bcbb89ae1987f3ba5dedc17a
Abdulrahim Al Methiab <aalmethiab@gmail.com>
2023-08-22 17:04:49 +0200

    format

--- bindings/nodejs/lib/types/block/mana-allotment.ts
+++ bindings/nodejs/lib/types/block/mana-allotment.ts
@@ -1,29 +1,26 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { AccountId } from "..";
+import { AccountId } from '..';
 
 /**
  * An allotment of Mana which will be added upon commitment of the slot in which the containing transaction was issued,
  * in the form of Block Issuance Credits to the account.
  */
 class ManaAllotment {
-  /**
-   * The Account to allot Mana to.
-   */
-  readonly accountId: AccountId;
-  /**
-   * The Amount of Mana to allot.
-   */
-  readonly mana: bigint;
+    /**
+     * The Account to allot Mana to.
+     */
+    readonly accountId: AccountId;
+    /**
+     * The Amount of Mana to allot.
+     */
+    readonly mana: bigint;
 
-  constructor(accountId: AccountId, mana: bigint) {
-    this.accountId = accountId;
-    this.mana = mana;
-  }
+    constructor(accountId: AccountId, mana: bigint) {
+        this.accountId = accountId;
+        this.mana = mana;
+    }
 }
 
-export {
-  ManaAllotment
-};
-
+export { ManaAllotment };
