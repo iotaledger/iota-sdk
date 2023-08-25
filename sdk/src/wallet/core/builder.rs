301c499462a3f4e9adda6b7ab3b135d2c0c57cb0
Sharang Parnerkar <parnerkarsharang@gmail.com>
2023-08-25 08:48:11 +0200

    Update sdk/src/wallet/core/builder.rs
    Co-authored-by: Alexandcoats <alexandcoats@gmail.com>

--- sdk/src/wallet/core/builder.rs
+++ sdk/src/wallet/core/builder.rs
@@ -133,16 +133,13 @@ where
         }
 
         #[cfg(feature = "storage")]
-        cfg_if::cfg_if!(
+        let storage = cfg_if::cfg_if!(
             if #[cfg(feature = "rocksdb")] {
-                let storage =
-                crate::wallet::storage::adapter::rocksdb::RocksdbStorageAdapter::new(storage_options.path.clone())?;
-            }
-            else if #[cfg(feature = "jammdb")]{
-                let storage = crate::wallet::storage::adapter::jammdb::JammdbStorageAdapter::new(storage_options.path.clone())?;
-            }
-            else {
-                let storage = crate::wallet::storage::adapter::memory::Memory::default();
+                crate::wallet::storage::adapter::rocksdb::RocksdbStorageAdapter::new(storage_options.path.clone())?
+            } else if #[cfg(feature = "jammdb")] {
+                crate::wallet::storage::adapter::jammdb::JammdbStorageAdapter::new(storage_options.path.clone())?
+            } else {
+                crate::wallet::storage::adapter::memory::Memory::default()
             }
         );
 
