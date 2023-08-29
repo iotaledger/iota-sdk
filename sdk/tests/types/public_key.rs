ca7bd7cb007c4a78d21d00a7a535c4130d6c684a
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2023-08-29 15:55:25 +0200

    Update sdk/tests/types/public_key.rs
    Co-authored-by: Alexandcoats <alexandcoats@gmail.com>

--- sdk/tests/types/public_key.rs
+++ sdk/tests/types/public_key.rs
@@ -5,7 +5,7 @@ use iota_sdk::types::block::public_key::PublicKey;
 
 #[test]
 #[cfg(feature = "serde")]
-fn public_key_de_serialization() {
+fn public_key_serde_roundtrip() {
     let serialized_str =
         r#"{"type":0,"publicKey":"0xfc5cef12850558d533a6c485051b4397fd9b4349f90ab62c58e3ea99f1f47d16"}"#;
     let ed25519_public_key: PublicKey = serde_json::from_str(serialized_str).unwrap();
