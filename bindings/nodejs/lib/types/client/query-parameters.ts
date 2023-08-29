14ed56e632b06c9f2f34b08bf230e811d9096d11
Brord van Wierst <brord@iota.org>
2023-08-29 13:07:51 +0200

    format

--- bindings/nodejs/lib/types/client/query-parameters.ts
+++ bindings/nodejs/lib/types/client/query-parameters.ts
@@ -1,7 +1,7 @@
 // Copyright 2021-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { SlotIndex } from "../block/slot";
+import { SlotIndex } from '../block/slot';
 
 /**
  * Query parameter for filtering output requests
