1f6b2f2df3fd6a18026e553660710bbf84516ab7
Pawel Iwan <pawel.ivan@iota.org>
2023-09-04 09:43:55 +0200

    remove expression

--- bindings/python/examples/wallet/12-prepare_output.py
+++ bindings/python/examples/wallet/12-prepare_output.py
@@ -19,7 +19,7 @@ wallet.set_stronghold_password(os.environ["STRONGHOLD_PASSWORD"])
 
 # using prepare_output
 output = account.prepare_output(OutputParams(
-    "rms1qprutadk4uc9rp6h7wh7sech42sl0z40ztpgyygr5tf0cn5jrqshgm8y43d", "1000000", unlocks=Unlocks(expiration_unix_time == 1676570528)))
+    "rms1qprutadk4uc9rp6h7wh7sech42sl0z40ztpgyygr5tf0cn5jrqshgm8y43d", "1000000", unlocks=Unlocks(expiration_unix_time = 1676570528)))
 print(f"Output: {json.dumps(output.to_dict(), indent=4)}")
 
 account.sync()
