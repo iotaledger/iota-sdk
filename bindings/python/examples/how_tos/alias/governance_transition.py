6c506e312c5fe75bca00785bf469135a2f93beaa
Thoralf Müller <thoralf.mue@gmail.com>
2023-09-18 17:11:18 +0200

    Comment like this?

--- bindings/python/examples/how_tos/alias/governance_transition.py
+++ bindings/python/examples/how_tos/alias/governance_transition.py
@@ -43,8 +43,10 @@ print(f"{new_state_controller.__dict__}")
 alias_output = alias_output_data.output
 
 
-# Replace the address in the StateControllerAddressUnlockCondition
 def update_state_controller(unlock_condition):
+    """
+    Replace the address in the StateControllerAddressUnlockCondition
+    """
     if unlock_condition.type == UnlockConditionType.StateControllerAddress:
         return StateControllerAddressUnlockCondition(new_state_controller)
     return unlock_condition
