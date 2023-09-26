f7309861f20db586a8891c6f8736d331ab80a588
Thibault Martinez <thibault.martinez.30@gmail.com>
2023-09-26 15:10:59 +0200

    Update sdk/src/types/block/mana/rewards.rs
    Co-authored-by: DaughterOfMars <alexandcoats@gmail.com>

--- sdk/src/types/block/mana/rewards.rs
+++ sdk/src/types/block/mana/rewards.rs
@@ -26,7 +26,7 @@ pub struct RewardsParameters {
     mana_share_coefficient: u64,
     /// The exponent used for calculation of the initial reward.
     decay_balancing_constant_exponent: u8,
-    /// Needs to be an integer approximation calculated based on chosen DecayBalancingConstantExponent.
+    /// An integer approximation which is calculated using the `decay_balancing_constant_exponent`.
     #[cfg_attr(feature = "serde", serde(with = "crate::utils::serde::string"))]
     decay_balancing_constant: u64,
     /// The exponent used for shifting operation during the pool rewards calculations.
