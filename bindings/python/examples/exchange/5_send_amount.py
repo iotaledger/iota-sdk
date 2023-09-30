52e6a44aa68a61b0ba59498e6ecebdcc076d2d4f
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-30 18:16:05 +0000

    cleanup env var checks

--- bindings/python/examples/exchange/5_send_amount.py
+++ bindings/python/examples/exchange/5_send_amount.py
@@ -13,12 +13,9 @@ from iota_sdk import SyncOptions, Wallet
 # should not be done in production.
 load_dotenv()
 
-if 'WALLET_DB_PATH' not in os.environ:
-    raise Exception(".env WALLET_DB_PATH is undefined, see .env.example")
-if 'STRONGHOLD_PASSWORD' not in os.environ:
-    raise Exception(".env STRONGHOLD_PASSWORD is undefined, see .env.example")
-if 'EXPLORER_URL' not in os.environ:
-    raise Exception(".env EXPLORER_URL is undefined, see .env.example")
+for env_var in ['WALLET_DB_PATH', 'STRONGHOLD_PASSWORD', 'EXPLORER_URL']:
+    if env_var not in os.environ:
+        raise Exception(f'.env {env_var} is undefined, see .env.example')
 
 wallet = Wallet(os.environ.get('WALLET_DB_PATH'))
 
