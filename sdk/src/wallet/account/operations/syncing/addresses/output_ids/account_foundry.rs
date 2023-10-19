e70a204a953e78be411b32bb0bfe25783927785e
Thibault Martinez <thibault@iota.org>
2023-10-19 15:39:19 +0200

    Remove TODO

--- sdk/src/wallet/account/operations/syncing/addresses/output_ids/account_foundry.rs
+++ sdk/src/wallet/account/operations/syncing/addresses/output_ids/account_foundry.rs
@@ -26,7 +26,6 @@ impl<S: 'static + SecretManage> Account<S>
 where
     crate::wallet::Error: From<S::Error>,
 {
-    // TODO
     /// Returns output ids of account outputs
     pub(crate) async fn get_account_and_foundry_output_ids(
         &self,
