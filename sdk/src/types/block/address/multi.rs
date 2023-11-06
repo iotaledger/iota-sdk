e427afe6ff0558ae02ba55351a9499e3b55de636
Thibault Martinez <thibault.martinez.30@gmail.com>
2023-11-06 14:28:01 +0100

    Update sdk/src/types/block/address/multi.rs
    Co-authored-by: DaughterOfMars <alexandcoats@gmail.com>

--- sdk/src/types/block/address/multi.rs
+++ sdk/src/types/block/address/multi.rs
@@ -84,7 +84,7 @@ pub struct MultiAddress {
 }
 
 impl MultiAddress {
-    /// The [`Address`](crate::types::block::address::Address) kind of an [`MultiAddress`].
+    /// The [`Address`](crate::types::block::address::Address) kind of a [`MultiAddress`].
     pub const KIND: u8 = 40;
     /// The allowed range of inner [`Address`]es.
     pub const ADDRESSES_COUNT: RangeInclusive<u8> = 1..=10;
