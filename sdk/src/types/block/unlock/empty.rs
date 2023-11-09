34673f77490d05281a4e98465df334e396e3be4b
Thibault Martinez <thibault@iota.org>
2023-11-09 12:29:19 +0100

    Fmt

--- sdk/src/types/block/unlock/empty.rs
+++ sdk/src/types/block/unlock/empty.rs
@@ -3,8 +3,8 @@
 
 use crate::types::block::Error;
 
-/// Used to maintain correct index relationship between addresses and signatures when unlocking a [`MultiAddress`](crate::types::block::address::MultiAddress) where
-/// not all addresses are unlocked.
+/// Used to maintain correct index relationship between addresses and signatures when unlocking a
+/// [`MultiAddress`](crate::types::block::address::MultiAddress) where not all addresses are unlocked.
 #[derive(Clone, Debug, Eq, PartialEq, Hash, packable::Packable)]
 pub struct EmptyUnlock;
 
