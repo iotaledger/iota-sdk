36d698501493db757dd5c76eb437824ba3bac626
Thibault Martinez <thibault@iota.org>
2023-11-15 10:15:38 +0100

    Remove commented code

--- sdk/src/types/block/output/mod.rs
+++ sdk/src/types/block/output/mod.rs
@@ -448,18 +448,6 @@ impl StorageScore for Output {
 
 impl MinimumOutputAmount for Output {}
 
-// pub(crate) fn verify_output_amount_supply(amount: u64, token_supply: u64) -> Result<(), Error> {
-//     if amount > token_supply {
-//         Err(Error::InvalidOutputAmount(amount))
-//     } else {
-//         Ok(())
-//     }
-// }
-
-// pub(crate) fn verify_output_amount(amount: u64, token_supply: u64) -> Result<(), Error> {
-//     verify_output_amount_supply(amount, token_supply)
-// }
-
 /// A trait that is shared by all output types, which is used to calculate the minimum amount the output
 /// must contain to satisfy its storage cost.
 pub trait MinimumOutputAmount: StorageScore {
