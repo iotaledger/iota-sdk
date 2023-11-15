884771ce1122581dd448fc92f049e764bb73a517
Thibault Martinez <thibault@iota.org>
2023-11-15 15:06:39 +0100

    Nit

--- sdk/src/types/block/payload/signed_transaction/transaction.rs
+++ sdk/src/types/block/payload/signed_transaction/transaction.rs
@@ -593,7 +593,7 @@ pub(crate) mod dto {
             let outputs = dto
                 .outputs
                 .into_iter()
-                .map(|o| Output::try_from(o))
+                .map(Output::try_from)
                 .collect::<Result<Vec<Output>, Error>>()?;
 
             let mut builder = Self::builder(network_id)
