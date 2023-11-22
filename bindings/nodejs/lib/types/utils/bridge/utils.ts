3fd67444a3341e7ea176359b6886dd703f5ff578
Thibault Martinez <thibault@iota.org>
2023-11-22 08:54:11 +0100

    Fix nodejs

--- bindings/nodejs/lib/types/utils/bridge/utils.ts
+++ bindings/nodejs/lib/types/utils/bridge/utils.ts
@@ -197,8 +197,8 @@ export interface __OutputHexBytes__ {
 // Modified `SlotCommitment` with bigint types converted to strings.
 type SlotCommitmentConverted = Omit<
     SlotCommitment,
-    'index' | 'cumulativeWeight'
-> & { index: string; cumulativeWeight: string };
+    'cumulativeWeight' | 'referenceManaCost'
+> & { cumulativeWeight: string, referenceManaCost: string };
 export interface __ComputeSlotCommitmentId__ {
     name: 'computeSlotCommitmentId';
     data: {
