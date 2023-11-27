b6d5a86c6d74a33589a42973f5d4189a388ac075
Thibault Martinez <thibault@iota.org>
2023-11-27 14:35:23 +0100

    Add assert_binary_response

--- sdk/tests/types/api/core.rs
+++ sdk/tests/types/api/core.rs
@@ -1,17 +1,26 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_sdk::types::block::BlockDto;
+use iota_sdk::types::block::{slot::SlotCommitment, BlockDto};
+use packable::PackableExt;
 use serde::Deserialize;
 
-fn assert_response<T>(path: &str)
+fn assert_json_response<T>(path: &str)
 where
     for<'a> T: Deserialize<'a>,
 {
-    serde_json::from_str::<T>(&std::fs::read_to_string(path).unwrap()).unwrap();
+    let file = std::fs::read_to_string(&format!("./tests/types/api/fixtures/{path}")).unwrap();
+    serde_json::from_str::<T>(&file).unwrap();
+}
+
+fn assert_binary_response<T: PackableExt>(path: &str, visitor: &T::UnpackVisitor) {
+    let file = std::fs::read_to_string(&format!("./tests/types/api/fixtures/{path}")).unwrap();
+    let bytes = hex::decode(file).unwrap();
+    T::unpack_verified(bytes, visitor).unwrap();
 }
 
 #[test]
 fn responses() {
-    assert_response::<BlockDto>("./tests/types/api/fixtures/get-block-by-id-empty-response-example.json");
+    // assert_json_response::<BlockDto>("get-block-by-id-empty-response-example.json");
+    assert_binary_response::<SlotCommitment>("get-commitment-response-binary-example", &());
 }
