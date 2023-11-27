bc59268b88f7a5859912773cdc06a1a99a323971
Thibault Martinez <thibault@iota.org>
2023-11-27 15:03:32 +0100

    Return result

--- sdk/tests/types/api/core.rs
+++ sdk/tests/types/api/core.rs
@@ -8,49 +8,55 @@ use iota_sdk::types::{
     },
     block::{output::OutputMetadata, slot::SlotCommitment, BlockDto, BlockId},
 };
-use packable::PackableExt;
+use packable::{
+    error::{UnexpectedEOF, UnpackError},
+    Packable, PackableExt,
+};
 use serde::Deserialize;
 
-fn assert_json_response<T>(path: &str)
+fn json_response<T>(path: &str) -> Result<T, serde_json::Error>
 where
     for<'a> T: Deserialize<'a>,
 {
     let file = std::fs::read_to_string(&format!("./tests/types/api/fixtures/{path}")).unwrap();
-    serde_json::from_str::<T>(&file).unwrap();
+    serde_json::from_str::<T>(&file)
 }
 
-fn assert_binary_response<T: PackableExt>(path: &str, visitor: &T::UnpackVisitor) {
+fn binary_response<T: PackableExt>(
+    path: &str,
+    visitor: &T::UnpackVisitor,
+) -> Result<T, UnpackError<<T as Packable>::UnpackError, UnexpectedEOF>> {
     let file = std::fs::read_to_string(&format!("./tests/types/api/fixtures/{path}")).unwrap();
     let bytes = hex::decode(file).unwrap();
-    T::unpack_verified(bytes, visitor).unwrap();
+    T::unpack_verified(bytes, visitor)
 }
 
 #[test]
 fn responses() {
-    assert_json_response::<BlockDto>("get-block-by-id-empty-response-example.json");
-    assert_json_response::<BlockDto>("get-block-by-id-response-example-confirmed-transaction.json");
-    assert_json_response::<BlockDto>("get-block-by-id-response-example-confirmed.json");
-    assert_json_response::<BlockDto>("get-block-by-id-response-example-conflicting-transaction.json");
-    assert_json_response::<BlockDto>("get-block-by-id-response-example-new-transaction.json");
-    assert_json_response::<BlockDto>("get-block-by-id-response-example-new.json");
-    assert_json_response::<BlockDto>("get-block-by-id-tagged-data-response-example.json");
-    assert_json_response::<BlockDto>("get-block-by-id-transaction-response-example.json");
-    assert_json_response::<BlockDto>("get-block-by-id-validation-response-example.json");
-    assert_json_response::<IssuanceBlockHeaderResponse>("get-buildingBlock-response-example.json");
-    assert_json_response::<SlotCommitment>("get-commitment-response-example.json");
-    assert_json_response::<CommitteeResponse>("get-committee-example.json");
-    assert_json_response::<CongestionResponse>("get-congestion-estimate-response-example.json");
-    // assert_json_response("get-full-output-metadata-example.json");
-    assert_json_response::<InfoResponse>("get-info-response-example.json");
-    assert_json_response::<ManaRewardsResponse>("get-mana-rewards-example.json");
-    assert_json_response::<OutputMetadata>("get-output-metadata-by-id-response-spent-example.json");
-    assert_json_response::<OutputMetadata>("get-output-metadata-by-id-response-unspent-example.json");
-    // assert_json_response("get-outputs-by-id-response-example.json");
-    assert_json_response::<RoutesResponse>("get-routes-response-example.json");
-    assert_json_response::<UtxoChangesResponse>("get-utxo-changes-response-example.json");
-    assert_json_response::<ValidatorResponse>("get-validator-example.json");
-    assert_json_response::<ValidatorsResponse>("get-validators-example.json");
-    assert_json_response::<BlockId>("post-blocks-response-example.json");
+    json_response::<BlockDto>("get-block-by-id-empty-response-example.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-response-example-confirmed-transaction.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-response-example-confirmed.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-response-example-conflicting-transaction.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-response-example-new-transaction.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-response-example-new.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-tagged-data-response-example.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-transaction-response-example.json").unwrap();
+    json_response::<BlockDto>("get-block-by-id-validation-response-example.json").unwrap();
+    json_response::<IssuanceBlockHeaderResponse>("get-buildingBlock-response-example.json").unwrap();
+    json_response::<SlotCommitment>("get-commitment-response-example.json").unwrap();
+    json_response::<CommitteeResponse>("get-committee-example.json").unwrap();
+    json_response::<CongestionResponse>("get-congestion-estimate-response-example.json").unwrap();
+    // json_response("get-full-output-metadata-example.json").unwrap();
+    json_response::<InfoResponse>("get-info-response-example.json").unwrap();
+    json_response::<ManaRewardsResponse>("get-mana-rewards-example.json").unwrap();
+    json_response::<OutputMetadata>("get-output-metadata-by-id-response-spent-example.json").unwrap();
+    json_response::<OutputMetadata>("get-output-metadata-by-id-response-unspent-example.json").unwrap();
+    // json_response("get-outputs-by-id-response-example.json").unwrap();
+    json_response::<RoutesResponse>("get-routes-response-example.json").unwrap();
+    json_response::<UtxoChangesResponse>("get-utxo-changes-response-example.json").unwrap();
+    json_response::<ValidatorResponse>("get-validator-example.json").unwrap();
+    json_response::<ValidatorsResponse>("get-validators-example.json").unwrap();
+    json_response::<BlockId>("post-blocks-response-example.json").unwrap();
 
-    assert_binary_response::<SlotCommitment>("get-commitment-response-binary-example", &());
+    binary_response::<SlotCommitment>("get-commitment-response-binary-example", &()).unwrap();
 }
