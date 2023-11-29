1f85113d76aa5962a8000106e27fbdbc107fec6e
Thibault Martinez <thibault@iota.org>
2023-11-29 17:24:18 +0100

    Fixes

--- sdk/src/types/block/rand/output/metadata.rs
+++ sdk/src/types/block/rand/output/metadata.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::types::block::{
-    output::{OutputConsumptionMetadata, OutputInclusionMetadata, OutputMetadata},
+    output::{OutputConsumptionMetadata, OutputId, OutputInclusionMetadata, OutputMetadata},
     rand::{
         block::rand_block_id,
         option::rand_option,
@@ -30,13 +30,18 @@ pub fn rand_output_consumption_metadata() -> OutputConsumptionMetadata {
     )
 }
 
-/// Generates a random [`OutputMetadata`].
-pub fn rand_output_metadata() -> OutputMetadata {
+/// Generates a random [`OutputMetadata`] with a specific [`OutputId`].
+pub fn rand_output_metadata_with_id(output_id: OutputId) -> OutputMetadata {
     OutputMetadata::new(
-        rand_output_id(),
+        output_id,
         rand_block_id(),
         rand_output_inclusion_metadata(),
         rand_option(rand_output_consumption_metadata()),
         rand_slot_commitment_id(),
     )
+}
+
+/// Generates a random [`OutputMetadata`].
+pub fn rand_output_metadata() -> OutputMetadata {
+    rand_output_metadata_with_id(rand_output_id())
 }
