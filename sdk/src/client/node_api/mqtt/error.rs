5c870db51be768cc37a354fa88e1482cd0bd8642
Brord van Wierst <brord@iota.org>
2023-11-29 23:57:28 +0100

    removed mqtt custom serde

--- sdk/src/client/node_api/mqtt/error.rs
+++ sdk/src/client/node_api/mqtt/error.rs
@@ -1,8 +1,6 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use serde::{ser::SerializeMap, Serialize, Serializer};
-
 /// MQTT related errors.
 #[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
@@ -19,25 +17,4 @@ pub enum Error {
     /// Invalid topic.
     #[error("invalid topic {0}")]
     InvalidTopic(String),
-}
-
-// Serialize type with Display error.
-impl Serialize for Error {
-    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut seq = serializer.serialize_map(Some(2))?;
-        let mut kind_dbg = format!("{self:?}");
-        // Convert first char to lowercase
-        if let Some(r) = kind_dbg.get_mut(0..1) {
-            r.make_ascii_lowercase();
-        }
-        // Split by whitespace for struct variants and split by `(` for tuple variants
-        // Safe to unwrap because kind_dbg is never an empty string
-        let kind = kind_dbg.split([' ', '(']).next().unwrap();
-        seq.serialize_entry("type", &kind)?;
-        seq.serialize_entry("error", &self.to_string())?;
-        seq.end()
-    }
 }
