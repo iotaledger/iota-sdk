827f80535e2242f28ac98656e5c8b72a24d02805
Thibault Martinez <thibault@iota.org>
2023-11-29 11:41:55 +0100

    Some changes to OutputMetadata

--- sdk/src/types/block/output/metadata.rs
+++ sdk/src/types/block/output/metadata.rs
@@ -2,24 +2,40 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::types::block::{
-    output::OutputId, payload::signed_transaction::TransactionId, slot::SlotCommitmentId, BlockId,
+    output::OutputId, payload::signed_transaction::TransactionId, slot::SlotCommitmentId, slot::SlotIndex, BlockId,
 };
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct OutputInclusionMetadata {
+    // Slot in which the output was included.
+    slot: SlotIndex,
+    // Transaction ID that created the output.
+    transaction_id: TransactionId,
+    // Commitment ID that includes the creation of the output.
+    commitment_id: Option<SlotCommitmentId>, // `serix:",omitempty"`
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct OutputConsumptionMetadata {
+    // Slot in which the output was spent.
+    slot: SlotIndex,
+    // Transaction ID that spent the output.
+    transaction_id: TransactionId,
+    // Commitment ID that includes the spending of the output.
+    commitment_id: Option<SlotCommitmentId>, // `serix:",omitempty"`
+}
 
 /// Metadata of an [`Output`](crate::types::block::output::Output).
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub struct OutputMetadata {
-    /// The ID of the block in which the output was included.
-    block_id: BlockId,
     /// The ID of the output.
     output_id: OutputId,
-    /// Whether the output is spent or not.
-    is_spent: bool,
-    // Commitment ID that includes the spent output.
-    commitment_id_spent: Option<SlotCommitmentId>,
-    // Transaction ID that spent the output.
-    transaction_id_spent: Option<TransactionId>,
-    /// Commitment ID that includes the output.
-    included_commitment_id: Option<SlotCommitmentId>,
+    /// The ID of the block in which the output was included.
+    block_id: BlockId,
+    // Metadata of the output if it is included in the ledger.
+    included: OutputInclusionMetadata,
+    // Metadata of the output if it is marked as spent in the ledger.
+    spent: Option<OutputConsumptionMetadata>, // `serix:",optional,omitempty"`
     /// Latest commitment ID of the node.
     latest_commitment_id: SlotCommitmentId,
 }
@@ -28,28 +44,19 @@ impl OutputMetadata {
     /// Creates a new [`OutputMetadata`].
     #[allow(clippy::too_many_arguments)]
     pub fn new(
-        block_id: BlockId,
         output_id: OutputId,
-        is_spent: bool,
-        commitment_id_spent: Option<SlotCommitmentId>,
-        transaction_id_spent: Option<TransactionId>,
-        included_commitment_id: Option<SlotCommitmentId>,
+        block_id: BlockId,
+        included: OutputInclusionMetadata,
+        spent: Option<OutputConsumptionMetadata>,
         latest_commitment_id: SlotCommitmentId,
     ) -> Self {
         Self {
             block_id,
             output_id,
-            is_spent,
-            commitment_id_spent,
-            transaction_id_spent,
-            included_commitment_id,
+            included,
+            spent,
             latest_commitment_id,
         }
-    }
-
-    /// Returns the block ID of the [`OutputMetadata`].
-    pub fn block_id(&self) -> &BlockId {
-        &self.block_id
     }
 
     /// Returns the output ID of the [`OutputMetadata`].
@@ -57,7 +64,6 @@ impl OutputMetadata {
         &self.output_id
     }
 
-    /// Returns the transaction ID of the [`OutputMetadata`].
     pub fn transaction_id(&self) -> &TransactionId {
         self.output_id.transaction_id()
     }
@@ -67,30 +73,35 @@ impl OutputMetadata {
         self.output_id.index()
     }
 
+    /// Returns the block ID of the [`OutputMetadata`].
+    pub fn block_id(&self) -> &BlockId {
+        &self.block_id
+    }
+
     /// Returns whether the output is spent or not.
     pub fn is_spent(&self) -> bool {
-        self.is_spent
+        self.spent.is_some()
     }
 
-    /// Sets whether the output is spent or not.
-    pub fn set_spent(&mut self, spent: bool) {
-        self.is_spent = spent;
-    }
+    // /// Sets whether the output is spent or not.
+    // pub fn set_spent(&mut self, spent: bool) {
+    //     self.is_spent = spent;
+    // }
 
-    /// Returns the commitment ID spent of the [`OutputMetadata`].
-    pub fn commitment_id_spent(&self) -> Option<&SlotCommitmentId> {
-        self.commitment_id_spent.as_ref()
-    }
+    // /// Returns the commitment ID spent of the [`OutputMetadata`].
+    // pub fn commitment_id_spent(&self) -> Option<&SlotCommitmentId> {
+    //     self.commitment_id_spent.as_ref()
+    // }
 
-    /// Returns the transaction ID spent of the [`OutputMetadata`].
-    pub fn transaction_id_spent(&self) -> Option<&TransactionId> {
-        self.transaction_id_spent.as_ref()
-    }
+    // /// Returns the transaction ID spent of the [`OutputMetadata`].
+    // pub fn transaction_id_spent(&self) -> Option<&TransactionId> {
+    //     self.transaction_id_spent.as_ref()
+    // }
 
-    /// Returns the included commitment ID of the [`OutputMetadata`].
-    pub fn included_commitment_id(&self) -> Option<&SlotCommitmentId> {
-        self.included_commitment_id.as_ref()
-    }
+    // /// Returns the included commitment ID of the [`OutputMetadata`].
+    // pub fn included_commitment_id(&self) -> Option<&SlotCommitmentId> {
+    //     self.included_commitment_id.as_ref()
+    // }
 
     /// Returns the latest commitment ID of the [`OutputMetadata`].
     pub fn latest_commitment_id(&self) -> &SlotCommitmentId {
@@ -107,9 +118,8 @@ mod dto {
     #[derive(Serialize, Deserialize)]
     #[serde(rename_all = "camelCase")]
     struct OutputMetadataDto {
+        output_id: OutputId,
         block_id: BlockId,
-        transaction_id: TransactionId,
-        output_index: u16,
         is_spent: bool,
         #[serde(default, skip_serializing_if = "Option::is_none")]
         commitment_id_spent: Option<SlotCommitmentId>,
@@ -123,8 +133,8 @@ mod dto {
     impl From<OutputMetadataDto> for OutputMetadata {
         fn from(value: OutputMetadataDto) -> Self {
             Self {
+                output_id: value.output_id,
                 block_id: value.block_id,
-                output_id: OutputId::new(value.transaction_id, value.output_index),
                 is_spent: value.is_spent,
                 commitment_id_spent: value.commitment_id_spent,
                 transaction_id_spent: value.transaction_id_spent,
@@ -137,9 +147,8 @@ mod dto {
     impl From<&OutputMetadata> for OutputMetadataDto {
         fn from(value: &OutputMetadata) -> Self {
             Self {
+                output_id: value.output_id,
                 block_id: value.block_id,
-                transaction_id: *value.transaction_id(),
-                output_index: value.output_index(),
                 is_spent: value.is_spent,
                 commitment_id_spent: value.commitment_id_spent,
                 transaction_id_spent: value.transaction_id_spent,
