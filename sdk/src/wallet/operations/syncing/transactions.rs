e39459d4ce47ce97e0746d54a59fc18f1a543d12
Thibault Martinez <thibault@iota.org>
2023-11-30 11:40:49 +0100

    Some API changes (#1714)
    * Some API changes
    
    * Fix compilation
    
    * Some changes to OutputMetadata
    
    * Remove DTO
    
    * Fix rand
    
    * Fixes
    
    * Fix test
    
    * Finishing touches
    
    * Add getters
    
    * Nit
    
    * Nits
    
    * is_spent
    
    * Update sdk/src/types/api/core.rs
    
    Co-authored-by: /alex/ <alexander.schmidt@iota.org>
    
    * Clean TODO
    
    * Update sdk/src/types/api/core.rs
    
    Co-authored-by: /alex/ <alexander.schmidt@iota.org>
    
    ---------
    
    Co-authored-by: /alex/ <alexander.schmidt@iota.org>

--- sdk/src/wallet/operations/syncing/transactions.rs
+++ sdk/src/wallet/operations/syncing/transactions.rs
@@ -101,7 +101,7 @@ where
             if let Some(block_id) = transaction.block_id {
                 match self.client().get_block_metadata(&block_id).await {
                     Ok(metadata) => {
-                        if let Some(tx_state) = metadata.transaction_state {
+                        if let Some(tx_state) = metadata.transaction_metadata.map(|m| m.transaction_state) {
                             match tx_state {
                                 // TODO: Separate TransactionState::Finalized?
                                 TransactionState::Finalized | TransactionState::Confirmed => {
