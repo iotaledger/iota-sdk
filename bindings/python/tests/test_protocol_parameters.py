7037102484da14589580831a460f4485deed25f0
Alexander Schmidt <alexander.schmidt@iota.org>
2023-12-01 23:20:37 +0100

    add python hash check

--- bindings/python/tests/test_protocol_parameters.py
+++ bindings/python/tests/test_protocol_parameters.py
@@ -2,15 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import json
-from dacite import from_dict
-from iota_sdk import ProtocolParameters
+from iota_sdk import ProtocolParameters, Utils
 
 
-protocol_params = {}
+protocol_params_json = {}
 with open('../../sdk/tests/types/fixtures/protocol_parameters.json', "r", encoding="utf-8") as json_file:
-    protocol_params = json.load(json_file)
+    protocol_params_json = json.load(json_file)
 
 def test_protocol_parameters():
-    protocol_params_dict = protocol_params['params']
+    protocol_params_dict = protocol_params_json['params']
     protocol_params = ProtocolParameters.from_dict(protocol_params_dict)
     assert protocol_params.to_dict() == protocol_params_dict
+
+    expected_hash = protocol_params_json['hash']
+    assert Utils.protocol_parameters_hash(protocol_params) == expected_hash
