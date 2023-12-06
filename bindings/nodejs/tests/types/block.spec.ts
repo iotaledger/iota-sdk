beb9f627d4a76e626a71e616eec798ede587a9e1
Alexander Schmidt <alexander.schmidt@iota.org>
2023-12-06 15:59:25 +0100

    nodejs: validation block

--- bindings/nodejs/tests/types/block.spec.ts
+++ bindings/nodejs/tests/types/block.spec.ts
@@ -8,7 +8,7 @@ import * as basic_block_tagged_data_payload_json from '../../../../sdk/tests/typ
 import * as basic_block_transaction_payload_json from '../../../../sdk/tests/types/fixtures/basic_block_transaction_payload.json';
 import * as validation_block_json from '../../../../sdk/tests/types/fixtures/validation_block.json';
 import * as protocol_parameters from '../../../../sdk/tests/types/fixtures/protocol_parameters.json';
-import { BasicBlockBody, Utils, ProtocolParameters, Block, HexEncodedString, TaggedDataPayload, Transaction, PayloadType, SignedTransactionPayload } from '../../';
+import { BasicBlockBody, Utils, ProtocolParameters, Block, HexEncodedString, TaggedDataPayload, Transaction, PayloadType, SignedTransactionPayload, ValidationBlockBody } from '../../';
 
 describe('Block tests', () => {
 
@@ -45,6 +45,19 @@ describe('Block tests', () => {
 
         const params = protocol_parameters.params as unknown as ProtocolParameters;
         const expected_id = basic_block_transaction_payload_json.id as unknown as HexEncodedString;
+        // TODO: should we add an id() method on Block like we have in Python?
+        const id = Utils.blockId(block, params);
+        expect(id).toEqual(expected_id);
+    });
+
+    it('compares validation block from a fixture', async () => {
+        const block = validation_block_json.block as unknown as Block;
+        expect(block.isValidation).toBe(true);
+        expect(block.body.isValidation).toBe(true);
+        expect(block.asValidation).toBeInstanceOf(ValidationBlockBody);
+
+        const params = protocol_parameters.params as unknown as ProtocolParameters;
+        const expected_id = validation_block_json.id as unknown as HexEncodedString;
         // TODO: should we add an id() method on Block like we have in Python?
         const id = Utils.blockId(block, params);
         expect(id).toEqual(expected_id);
