6bacf198a0afe52bbc45edc7290331b38967eb80
Brord van Wierst <brord@iota.org>
2023-12-11 17:37:34 +0100

    added protocol params to sign

--- bindings/python/iota_sdk/secret_manager/secret_manager.py
+++ bindings/python/iota_sdk/secret_manager/secret_manager.py
@@ -8,6 +8,7 @@ import humps
 from iota_sdk.external import create_secret_manager, call_secret_manager_method
 from iota_sdk.types.block.signed_block import SignedBlock, UnsignedBlock
 from iota_sdk.types.common import HexStr
+from iota_sdk.types.node_info import ProtocolParameters
 from iota_sdk.types.signature import Ed25519Signature, Bip44
 from iota_sdk.types.transaction_data import PreparedTransactionData
 from iota_sdk.types.payload import SignedTransactionPayload
@@ -268,14 +269,16 @@ class SecretManager():
         })
 
     def sign_transaction(
-            self, prepared_transaction_data: PreparedTransactionData) -> SignedTransactionPayload:
+            self, prepared_transaction_data: PreparedTransactionData, protocol_parameters: ProtocolParameters) -> SignedTransactionPayload:
         """Sign a transaction.
 
         Args:
             prepare_transaction_data: The prepared transaction data that needs to be signed.
+            protocol_parameters: The protocol parameters used in creating the signed transaction.
         """
         return SignedTransactionPayload.from_dict(self._call_method('signTransaction', {
-            'preparedTransactionData': prepared_transaction_data.to_dict()
+            'preparedTransactionData': prepared_transaction_data.to_dict(),
+            'protocolParameters': protocol_parameters
         }))
 
     def sign_block(
