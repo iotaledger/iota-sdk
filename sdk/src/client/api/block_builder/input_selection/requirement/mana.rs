002e7b7e85bcc11eba77207b4cecec134d7be925
Thibault Martinez <thibault@iota.org>
2023-12-11 10:59:58 +0100

    Already fulfilled check

--- sdk/src/client/api/block_builder/input_selection/requirement/mana.rs
+++ sdk/src/client/api/block_builder/input_selection/requirement/mana.rs
@@ -6,6 +6,16 @@ use crate::client::secret::types::InputSigningData;
 
 impl InputSelection {
     pub(crate) fn fulfill_mana_requirement(&mut self, allotments: u64) -> Result<Vec<InputSigningData>, Error> {
+        let required_mana = self.outputs.iter().map(|o| o.mana()).sum::<u64>() + allotments;
+
+        // Checks if the requirement is already fulfilled.
+        let selected_mana = self.selected_inputs.iter().map(|o| o.output.mana()).sum::<u64>();
+
+        if selected_mana >= required_mana {
+            log::debug!("Mana requirement already fulfilled");
+            return Ok(Vec::new());
+        }
+
         Ok(vec![])
     }
 }
