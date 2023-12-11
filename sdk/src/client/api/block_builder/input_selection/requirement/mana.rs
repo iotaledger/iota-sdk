14c12d3f3bb2ce7a62ee470cf6ecb3168ee772af
Thibault Martinez <thibault@iota.org>
2023-12-11 11:17:32 +0100

    Select inputs for mana

--- sdk/src/client/api/block_builder/input_selection/requirement/mana.rs
+++ sdk/src/client/api/block_builder/input_selection/requirement/mana.rs
@@ -7,15 +7,24 @@ use crate::client::secret::types::InputSigningData;
 impl InputSelection {
     pub(crate) fn fulfill_mana_requirement(&mut self, allotments: u64) -> Result<Vec<InputSigningData>, Error> {
         let required_mana = self.outputs.iter().map(|o| o.mana()).sum::<u64>() + allotments;
-
-        // Checks if the requirement is already fulfilled.
-        let selected_mana = self.selected_inputs.iter().map(|o| o.output.mana()).sum::<u64>();
+        let mut selected_mana = self.selected_inputs.iter().map(|o| o.output.mana()).sum::<u64>();
 
         if selected_mana >= required_mana {
             log::debug!("Mana requirement already fulfilled");
-            return Ok(Vec::new());
-        }
+            Ok(Vec::new())
+        } else {
+            let mut inputs = Vec::new();
 
-        Ok(vec![])
+            while let Some(input) = self.available_inputs.pop() {
+                selected_mana += input.output.mana();
+                inputs.push(input);
+
+                if selected_mana >= required_mana {
+                    break;
+                }
+            }
+
+            Ok(inputs)
+        }
     }
 }
