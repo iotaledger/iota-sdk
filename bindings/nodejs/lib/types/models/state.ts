4d4564eb4f2a302617ba3d0d0460751d3bec5b09
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2024-01-09 14:04:36 +0100

    Apply suggestions from code review
    Co-authored-by: Thibault Martinez <thibault.martinez.30@gmail.com>

--- bindings/nodejs/lib/types/models/state.ts
+++ bindings/nodejs/lib/types/models/state.ts
@@ -15,9 +15,9 @@ export declare type BlockState =
  * The different states of a transaction.
  * If 'pending', the transaction is not included yet.
  * If 'accepted', the transaction is included.
- * If 'confirmed' the transaction is included and its included block is confirmed.
- * If 'finalized' the transaction is included, its included block is finalized and cannot be reverted anymore.
- * If 'failed' the transaction is not successfully issued due to failure reason.
+ * If 'confirmed', the transaction is included and its included block is confirmed.
+ * If 'finalized', the transaction is included, its included block is finalized and cannot be reverted anymore.
+ * If 'failed', the transaction is not successfully issued due to failure reason.
  */
 export declare type TransactionState =
     | 'pending'
