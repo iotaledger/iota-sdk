1ea105428d21d74a8440c4520f323b88beccf48b
Thoralf Müller <thoralf.mue@gmail.com>
2024-01-09 18:32:41 +0100

    Consider backed ed25519 addresses in more places in the ISA

--- sdk/src/client/api/block_builder/input_selection/requirement/ed25519.rs
+++ sdk/src/client/api/block_builder/input_selection/requirement/ed25519.rs
@@ -27,7 +27,9 @@ impl InputSelection {
             .unwrap()
             .expect("expiration unlockable outputs already filtered out");
 
-        &required_address == address
+        required_address
+            .backing_ed25519()
+            .map_or(false, |a| &Address::Ed25519(*a) == address)
     }
 
     /// Fulfills an ed25519 sender requirement by selecting an available input that unlocks its address.
