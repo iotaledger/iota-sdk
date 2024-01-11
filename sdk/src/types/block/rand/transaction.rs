71b34ef3b603a2bd55a54ed62b1544ce7ebbd01b
Thibault Martinez <thibault@iota.org>
2024-01-11 10:09:21 +0100

    rand nit

--- sdk/src/types/block/rand/transaction.rs
+++ sdk/src/types/block/rand/transaction.rs
@@ -3,16 +3,16 @@
 
 use crate::types::block::{
     payload::signed_transaction::{TransactionHash, TransactionId},
-    rand::bytes::rand_bytes_array,
+    rand::{bytes::rand_bytes_array, number::rand_number},
     slot::SlotIndex,
 };
-
-/// Generates a random transaction id.
-pub fn rand_transaction_id() -> TransactionId {
-    TransactionId::new(rand_bytes_array())
-}
 
 /// Generates a random transaction id with a given slot index.
 pub fn rand_transaction_id_with_slot_index(slot_index: impl Into<SlotIndex>) -> TransactionId {
     TransactionHash::new(rand_bytes_array()).into_transaction_id(slot_index.into())
+}
+
+/// Generates a random transaction id.
+pub fn rand_transaction_id() -> TransactionId {
+    rand_transaction_id_with_slot_index(rand_number::<u32>())
 }
