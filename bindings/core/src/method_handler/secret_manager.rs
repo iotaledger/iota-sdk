1947f54b3b808228f8853b22b21719b50ff9e980
marc2332 <mespinsanz@gmail.com>
2024-01-11 12:29:25 +0100

    feat(2.0, nodejs): Add changeStrongholdPassword for SecretManager

--- bindings/core/src/method_handler/secret_manager.rs
+++ bindings/core/src/method_handler/secret_manager.rs
@@ -147,6 +147,18 @@ where
             stronghold.set_password(password).await?;
             Response::Ok
         }
+        #[cfg(feature = "stronghold")]
+        SecretManagerMethod::ChangeStrongholdPassword { password } => {
+            let stronghold = if let Some(secret_manager) = secret_manager.downcast::<StrongholdSecretManager>() {
+                secret_manager
+            } else if let Some(SecretManager::Stronghold(secret_manager)) = secret_manager.downcast::<SecretManager>() {
+                secret_manager
+            } else {
+                return Err(iota_sdk::client::Error::SecretManagerMismatch.into());
+            };
+            stronghold.change_password(password).await?;
+            Response::Ok
+        }
     };
     Ok(response)
 }
