1e996414d01cd68d197e59971f535907fdbbc904
Thoralf Müller <thoralf.mue@gmail.com>
2024-01-12 20:56:24 +0100

    Add MetadataFeature tests

---
+++ sdk/tests/types/output/feature/metadata.rs
@@ -0,0 +1,56 @@
+// Copyright 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use iota_sdk::types::block::{output::feature::MetadataFeature, Error};
+use packable::{error::UnpackError, PackableExt};
+
+#[test]
+fn invalid() {
+    // Invalid key
+    assert!(
+        serde_json::from_str::<MetadataFeature>(
+            r#"{"type": 2, "entries": { "space is a non graphical ASCII value": "0x42" } }"#
+        )
+        .is_err()
+    );
+
+    // Invalid value
+    assert!(serde_json::from_str::<MetadataFeature>(r#"{"type": 2, "entries": { "nothing": "" } }"#).is_err());
+}
+
+#[test]
+fn serde_roundtrip() {
+    // Single entry
+    let metadata_feature: MetadataFeature =
+        serde_json::from_str(r#"{"type": 2, "entries": { "some_key": "0x42" } }"#).unwrap();
+    let metadata_feature_ser = serde_json::to_string(&metadata_feature).unwrap();
+
+    assert_eq!(
+        serde_json::from_str::<MetadataFeature>(&metadata_feature_ser).unwrap(),
+        metadata_feature
+    );
+
+    // Multiple entries, order doesn't matter
+    let metadata_feature: MetadataFeature =
+        serde_json::from_str(r#"{"type": 2, "entries": { "b": "0x42", "a": "0x1337", "c": "0x" } }"#).unwrap();
+    let metadata_feature_ser = serde_json::to_string(&metadata_feature).unwrap();
+
+    assert_eq!(
+        serde_json::from_str::<MetadataFeature>(&metadata_feature_ser).unwrap(),
+        metadata_feature
+    );
+}
+
+#[test]
+fn unpack_invalid_order() {
+    assert!(matches!(
+        MetadataFeature::unpack_verified([3, 0, 1, 99, 0, 0, 1, 98, 0, 0, 1, 97, 0, 0], &()),
+        Err(UnpackError::Packable(Error::InvalidMetadataFeature(error_msg))) if &error_msg == "unordered map"
+    ));
+}
+
+#[test]
+fn unpack_invalid_length() {
+    // TODO: this should fail
+    MetadataFeature::unpack_verified([vec![1, 0, 1, 33, 0, 32], vec![0u8; 8192]].concat(), &()).unwrap();
+}
