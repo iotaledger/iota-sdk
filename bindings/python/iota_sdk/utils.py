cacac7c178a411ebfaf79b274ca391508f800666
Thoralf Müller <thoralf.mue@gmail.com>
2024-01-24 10:25:38 +0100

    Remove unused import

--- bindings/python/iota_sdk/utils.py
+++ bindings/python/iota_sdk/utils.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from __future__ import annotations
-from json import dumps, loads
 import json
 from typing import TYPE_CHECKING, List, Optional
 from iota_sdk.common import custom_encoder
