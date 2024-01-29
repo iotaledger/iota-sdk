abcdd6173f0a3bd656a5fc2e0b6381bc7905f9e1
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2024-01-29 10:53:19 +0100

    Update delegation creation and transition rules (#1421)
    * Add end_epoch delegation transition rules
    
    * u32 slot index
    
    * no std
    
    * Remove todo
    
    * Review comments, fix SlotCommitmentId length, update tests
    
    * Resolve conflicts
    
    * Remove epoch_start in favor of first_slot_index
    
    * fmt
    
    * Review suggestions
    
    * Resolve conflicts
    
    * Review suggestions
    
    * Cleanup
    
    * Update creation check
    
    * Address review comments
    
    * Use max_committable_age
    
    * Move things, clarify
    
    * Move bounded_slot methods to SlotCommitmentId
    
    * typo
    
    * Simplify
    
    * Add genesis slot offset to slot calculations
    
    * Accept impl Into<SlotIndex>
    
    * Apply suggestions from code review
    
    Co-authored-by: Thibault Martinez <thibault.martinez.30@gmail.com>
    
    * impl Into<SlotIndex> for consistency
    
    * Align param order
    
    * Remove into
    
    * Align registration slot calculation with TIP
    
    * Add genesis_slot offset to range
    
    * Fmt, no_std
    
    * Clippy
    
    ---------
    
    Co-authored-by: Thibault Martinez <thibault.martinez.30@gmail.com>

--- sdk/src/types/block/slot/index.rs
+++ sdk/src/types/block/slot/index.rs
@@ -46,12 +46,17 @@ pub struct SlotIndex(pub u32);
 
 impl SlotIndex {
     /// Gets the [`EpochIndex`] of this slot.
-    pub fn to_epoch_index(self, slots_per_epoch_exponent: u8) -> EpochIndex {
-        EpochIndex::from_slot_index(self, slots_per_epoch_exponent)
+    pub fn to_epoch_index(self, genesis_slot: impl Into<Self>, slots_per_epoch_exponent: u8) -> EpochIndex {
+        EpochIndex::from_slot_index(self, genesis_slot, slots_per_epoch_exponent)
     }
 
-    pub fn from_epoch_index(epoch_index: EpochIndex, slots_per_epoch_exponent: u8) -> Self {
-        Self(*epoch_index << slots_per_epoch_exponent)
+    /// Gets the first [`SlotIndex`] of the provided epoch.
+    pub fn from_epoch_index(
+        epoch_index: EpochIndex,
+        genesis_slot: impl Into<Self>,
+        slots_per_epoch_exponent: u8,
+    ) -> Self {
+        genesis_slot.into() + Self(*epoch_index << slots_per_epoch_exponent)
     }
 
     /// Gets the slot index of a unix timestamp in seconds.
