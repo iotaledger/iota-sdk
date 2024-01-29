f4ff4ddc88046b324f98d9d26f2b5d9626f21ade
Alex Coats <alexandcoats@gmail.com>
2024-01-29 08:46:17 -0500

    suggestion

--- sdk/src/wallet/operations/transaction/high_level/delegation/delay.rs
+++ sdk/src/wallet/operations/transaction/high_level/delegation/delay.rs
@@ -41,71 +41,70 @@ where
         delegation_id: DelegationId,
         reclaim_excess: bool,
     ) -> crate::wallet::Result<PreparedTransactionData> {
-        if let Some(delegation_output) = self.get_delegation_output(delegation_id).await {
-            let protocol_parameters = self.client().get_protocol_parameters().await?;
-            // Add a commitment context input with the latest slot commitment
-            let slot_commitment_id = self.client().get_info().await?.node_info.status.latest_commitment_id;
-            let context_input = ContextInput::from(CommitmentContextInput::new(slot_commitment_id));
-
-            let min_delegation_amount = delegation_output
-                .output
-                .minimum_amount(protocol_parameters.storage_score_parameters());
-
-            // In order to split the output, the amount must be at least twice the minimum for a delegation output
-            let can_split = delegation_output.output.amount() >= 2 * min_delegation_amount;
-
-            // TODO: Should we return an error if `reclaim_excess == true` and `can_reclaim == false`?
-            let can_reclaim = if reclaim_excess {
-                let min_basic_amount = BasicOutput::minimum_amount(
-                    delegation_output.output.as_delegation().address(),
-                    protocol_parameters.storage_score_parameters(),
-                );
-                delegation_output.output.amount() >= min_delegation_amount + min_basic_amount
-            } else {
-                false
-            };
-
-            let mut builder = DelegationOutputBuilder::from(delegation_output.output.as_delegation())
-                .with_delegation_id(delegation_id)
-                .with_end_epoch(protocol_parameters.expected_end_epoch(slot_commitment_id));
-
-            if can_split || can_reclaim {
-                builder = builder.with_minimum_amount(protocol_parameters.storage_score_parameters());
-            }
-
-            let output = builder.finish_output()?;
-
-            let mut outputs = Vec::new();
-
-            // If we can split and we aren't reclaiming, we will create a new delegation with those funds
-            if can_split && !reclaim_excess {
-                outputs.push(
-                    DelegationOutputBuilder::new_with_amount(
-                        delegation_output.output.amount() - output.amount(),
-                        DelegationId::null(),
-                        *output.as_delegation().validator_address(),
-                    )
-                    .with_start_epoch(protocol_parameters.expected_start_epoch(slot_commitment_id))
-                    .add_unlock_condition(AddressUnlockCondition::new(output.as_delegation().address().clone()))
-                    .finish_output()?,
-                );
-            }
-
-            outputs.push(output);
-
-            self.prepare_transaction(
-                outputs,
-                TransactionOptions {
-                    custom_inputs: Some(vec![delegation_output.output_id]),
-                    context_inputs: Some(vec![context_input]),
-                    ..Default::default()
-                },
-            )
-            .await
-        } else {
-            Err(crate::wallet::Error::DelegationTransitionFailed(format!(
+        let delegation_output = self.get_delegation_output(delegation_id).await.ok_or_else(|| {
+            crate::wallet::Error::DelegationTransitionFailed(format!(
                 "no delegation output found with id {delegation_id}"
-            )))
+            ))
+        })?;
+        let protocol_parameters = self.client().get_protocol_parameters().await?;
+        // Add a commitment context input with the latest slot commitment
+        let slot_commitment_id = self.client().get_info().await?.node_info.status.latest_commitment_id;
+        let context_input = ContextInput::from(CommitmentContextInput::new(slot_commitment_id));
+
+        let min_delegation_amount = delegation_output
+            .output
+            .minimum_amount(protocol_parameters.storage_score_parameters());
+
+        // In order to split the output, the amount must be at least twice the minimum for a delegation output
+        let can_split = delegation_output.output.amount() >= 2 * min_delegation_amount;
+
+        // TODO: Should we return an error if `reclaim_excess == true` and `can_reclaim == false`?
+        let can_reclaim = if reclaim_excess {
+            let min_basic_amount = BasicOutput::minimum_amount(
+                delegation_output.output.as_delegation().address(),
+                protocol_parameters.storage_score_parameters(),
+            );
+            delegation_output.output.amount() >= min_delegation_amount + min_basic_amount
+        } else {
+            false
+        };
+
+        let mut builder = DelegationOutputBuilder::from(delegation_output.output.as_delegation())
+            .with_delegation_id(delegation_id)
+            .with_end_epoch(protocol_parameters.expected_end_epoch(slot_commitment_id));
+
+        if can_split || can_reclaim {
+            builder = builder.with_minimum_amount(protocol_parameters.storage_score_parameters());
         }
+
+        let output = builder.finish_output()?;
+
+        let mut outputs = Vec::new();
+
+        // If we can split and we aren't reclaiming, we will create a new delegation with those funds
+        if can_split && !reclaim_excess {
+            outputs.push(
+                DelegationOutputBuilder::new_with_amount(
+                    delegation_output.output.amount() - output.amount(),
+                    DelegationId::null(),
+                    *output.as_delegation().validator_address(),
+                )
+                .with_start_epoch(protocol_parameters.expected_start_epoch(slot_commitment_id))
+                .add_unlock_condition(AddressUnlockCondition::new(output.as_delegation().address().clone()))
+                .finish_output()?,
+            );
+        }
+
+        outputs.push(output);
+
+        self.prepare_transaction(
+            outputs,
+            TransactionOptions {
+                custom_inputs: Some(vec![delegation_output.output_id]),
+                context_inputs: Some(vec![context_input]),
+                ..Default::default()
+            },
+        )
+        .await
     }
 }
