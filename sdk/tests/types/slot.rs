678de8c54ffe5993bc3d2231654c209ce273b537
Thibault Martinez <thibault@iota.org>
2024-01-30 21:03:01 +0100

    SlotCommitmentFixture

--- sdk/tests/types/slot.rs
+++ sdk/tests/types/slot.rs
@@ -1,25 +1,33 @@
 // Copyright 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_sdk::types::block::slot::SlotCommitment;
+use iota_sdk::types::block::slot::{SlotCommitment, SlotCommitmentId};
 use packable::PackableExt;
 use pretty_assertions::assert_eq;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct SlotCommitmentFixture {
+    commitment: SlotCommitment,
+    bytes: String,
+    id: SlotCommitmentId,
+}
 
 #[test]
 fn slot_commitment_id_index() {
     // Test from https://github.com/iotaledger/tips-draft/blob/tip46/tips/TIP-0046/tip-0046.md#slot-commitment-id-1
-    let file = std::fs::read_to_string("./tests/types/fixtures/slot_commitment.json").unwrap();
-    let json = serde_json::from_str::<serde_json::Value>(&file).unwrap();
-    let slot_commitment_json = &json["commitment"];
-    let slot_commitment = serde_json::from_value::<SlotCommitment>(slot_commitment_json.clone()).unwrap();
-    let slot_commitment_bytes = slot_commitment.pack_to_vec();
-    let slot_commitment_id = slot_commitment.id();
+    let fixture = serde_json::from_value::<SlotCommitmentFixture>(
+        serde_json::from_reader(std::fs::File::open("./tests/types/fixtures/slot_commitment.json").unwrap()).unwrap(),
+    )
+    .unwrap();
+    let commitment_bytes = fixture.commitment.pack_to_vec();
+    let commitment_id = fixture.commitment.id();
 
-    assert_eq!(prefix_hex::encode(&slot_commitment_bytes), json["bytes"]);
+    assert_eq!(prefix_hex::encode(&commitment_bytes), fixture.bytes);
     assert_eq!(
-        slot_commitment,
-        SlotCommitment::unpack_unverified(slot_commitment_bytes).unwrap()
+        fixture.commitment,
+        SlotCommitment::unpack_verified(commitment_bytes, &()).unwrap()
     );
-    assert_eq!(slot_commitment_id.to_string(), json["id"]);
-    assert_eq!(slot_commitment_id.slot_index(), slot_commitment.slot());
+    assert_eq!(commitment_id, fixture.id);
+    assert_eq!(commitment_id.slot_index(), fixture.commitment.slot());
 }
