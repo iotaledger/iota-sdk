55d5f7dfeef00a2bbe8be763dbbf952fcbafd41c
Thibault Martinez <thibault@iota.org>
2024-02-05 14:55:22 +0100

    Adapt TransactionFailureReason to iota-core

--- sdk/src/types/block/semantic/error.rs
+++ sdk/src/types/block/semantic/error.rs
@@ -13,131 +13,142 @@ use crate::types::block::Error;
 #[packable(tag_type = u8, with_error = Error::InvalidTransactionFailureReason)]
 #[non_exhaustive]
 pub enum TransactionFailureReason {
-    /// The referenced UTXO was already spent.
-    InputUtxoAlreadySpent = 1,
-    /// The transaction is conflicting with another transaction. Conflicting specifically means a double spend
-    /// situation that both transaction pass all validation rules, eventually losing one(s) should have this reason.
-    ConflictingWithAnotherTx = 2,
-    /// The referenced UTXO is invalid.
-    InvalidReferencedUtxo = 3,
-    /// The transaction is invalid.
-    InvalidTransaction = 4,
-    /// The sum of the inputs and output base token amount does not match.
-    SumInputsOutputsAmountMismatch = 5,
-    /// The unlock block signature is invalid.
-    InvalidUnlockBlockSignature = 6,
-    /// The configured timelock is not yet expired.
-    TimelockNotExpired = 7,
-    /// The given native tokens are invalid.
-    InvalidNativeTokens = 8,
-    /// The return amount in a transaction is not fulfilled by the output side.
-    StorageDepositReturnUnfulfilled = 9,
-    /// An input unlock was invalid.
-    InvalidInputUnlock = 10,
-    /// The output contains a Sender with an ident (address) which is not unlocked.
-    SenderNotUnlocked = 11,
-    /// The chain state transition is invalid.
-    InvalidChainStateTransition = 12,
-    /// The referenced input is created after transaction issuing time.
-    InvalidTransactionIssuingTime = 13,
-    /// The mana amount is invalid.
-    InvalidManaAmount = 14,
-    /// The Block Issuance Credits amount is invalid.
-    InvalidBlockIssuanceCreditsAmount = 15,
-    /// Reward Context Input is invalid.
-    InvalidRewardContextInput = 16,
-    /// Commitment Context Input is invalid.
-    InvalidCommitmentContextInput = 17,
-    /// Staking Feature is not provided in account output when claiming rewards.
-    MissingStakingFeature = 18,
-    /// Failed to claim staking reward.
-    FailedToClaimStakingReward = 19,
-    /// Failed to claim delegation reward.
-    FailedToClaimDelegationReward = 20,
-    /// Burning of native tokens is not allowed in the transaction capabilities.
-    TransactionCapabilityNativeTokenBurningNotAllowed = 21,
-    /// Burning of mana is not allowed in the transaction capabilities.
-    TransactionCapabilityManaBurningNotAllowed = 22,
-    /// Destruction of accounts is not allowed in the transaction capabilities.
-    TransactionCapabilityAccountDestructionNotAllowed = 23,
-    /// Destruction of anchors is not allowed in the transaction capabilities.
-    TransactionCapabilityAnchorDestructionNotAllowed = 24,
-    /// Destruction of foundries is not allowed in the transaction capabilities.
-    TransactionCapabilityFoundryDestructionNotAllowed = 25,
-    /// Destruction of nfts is not allowed in the transaction capabilities.
-    TransactionCapabilityNftDestructionNotAllowed = 26,
-    /// The semantic validation failed for a reason not covered by the previous variants.
+    None = 0,
+    TypeInvalid = 1,
+    Conflicting = 2,
+    InputAlreadySpent = 3,
+    InputCreationAfterTxCreation = 4,
+    UnlockSignatureInvalid = 5,
+    CommitmentInputMissing = 6,
+    CommitmentInputReferenceInvalid = 7,
+    BICInputReferenceInvalid = 8,
+    RewardInputReferenceInvalid = 9,
+    StakingRewardCalculationFailure = 10,
+    DelegationRewardCalculationFailure = 11,
+    InputOutputBaseTokenMismatch = 12,
+    ManaOverflow = 13,
+    InputOutputManaMismatch = 14,
+    ManaDecayCreationIndexExceedsTargetIndex = 15,
+    NativeTokenAmountLessThanZero = 16,
+    NativeTokenSumExceedsUint256 = 17,
+    NativeTokenSumUnbalanced = 18,
+    MultiAddressLengthUnlockLengthMismatch = 19,
+    MultiAddressUnlockThresholdNotReached = 20,
+    NestedMultiUnlock = 21,
+    SenderFeatureNotUnlocked = 22,
+    IssuerFeatureNotUnlocked = 23,
+    StakingRewardInputMissing = 24,
+    StakingBlockIssuerFeatureMissing = 25,
+    StakingCommitmentInputMissing = 26,
+    StakingRewardClaimingInvalid = 27,
+    StakingFeatureRemovedBeforeUnbonding = 28,
+    StakingFeatureModifiedBeforeUnbonding = 29,
+    StakingStartEpochInvalid = 30,
+    StakingEndEpochTooEarly = 31,
+    BlockIssuerCommitmentInputMissing = 32,
+    BlockIssuanceCreditInputMissing = 33,
+    BlockIssuerNotExpired = 34,
+    BlockIssuerExpiryTooEarly = 35,
+    ManaMovedOffBlockIssuerAccount = 36,
+    AccountLocked = 37,
+    TimelockCommitmentInputMissing = 38,
+    TimelockNotExpired = 39,
+    ExpirationCommitmentInputMissing = 40,
+    ExpirationNotUnlockable = 41,
+    ReturnAmountNotFulFilled = 42,
+    NewChainOutputHasNonZeroedID = 43,
+    ChainOutputImmutableFeaturesChanged = 44,
+    ImplicitAccountDestructionDisallowed = 45,
+    MultipleImplicitAccountCreationAddresses = 46,
+    AccountInvalidFoundryCounter = 47,
+    FoundryTransitionWithoutAccount = 48,
+    FoundrySerialInvalid = 49,
+    DelegationCommitmentInputMissing = 50,
+    DelegationRewardInputMissing = 51,
+    DelegationRewardsClaimingInvalid = 52,
+    DelegationOutputTransitionedTwice = 53,
+    DelegationModified = 54,
+    DelegationStartEpochInvalid = 55,
+    DelegationAmountMismatch = 56,
+    DelegationEndEpochNotZero = 57,
+    DelegationEndEpochInvalid = 58,
+    CapabilitiesNativeTokenBurningNotAllowed = 59,
+    CapabilitiesManaBurningNotAllowed = 60,
+    CapabilitiesAccountDestructionNotAllowed = 61,
+    CapabilitiesAnchorDestructionNotAllowed = 62,
+    CapabilitiesFoundryDestructionNotAllowed = 63,
+    CapabilitiesNFTDestructionNotAllowed = 64,
     SemanticValidationFailed = 255,
 }
 
-impl fmt::Display for TransactionFailureReason {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::InputUtxoAlreadySpent => write!(f, "The referenced UTXO was already spent."),
-            Self::ConflictingWithAnotherTx => write!(
-                f,
-                "The transaction is conflicting with another transaction. Conflicting specifically means a double spend situation that both transactions pass all validation rules, eventually losing one(s) should have this reason."
-            ),
-            Self::InvalidReferencedUtxo => write!(f, "The referenced UTXO is invalid."),
-            Self::InvalidTransaction => write!(f, "The transaction is invalid."),
-            Self::SumInputsOutputsAmountMismatch => {
-                write!(f, "The sum of the inputs and output base token amount does not match.")
-            }
-            Self::InvalidUnlockBlockSignature => write!(f, "The unlock block signature is invalid."),
-            Self::TimelockNotExpired => write!(f, "The configured timelock is not yet expired."),
-            Self::InvalidNativeTokens => write!(f, "The given native tokens are invalid."),
-            Self::StorageDepositReturnUnfulfilled => write!(
-                f,
-                "The return amount in a transaction is not fulfilled by the output side."
-            ),
-            Self::InvalidInputUnlock => write!(f, "An input unlock was invalid."),
-            Self::SenderNotUnlocked => write!(
-                f,
-                "The output contains a Sender with an ident (address) which is not unlocked."
-            ),
-            Self::InvalidChainStateTransition => write!(f, "The chain state transition is invalid."),
-            Self::InvalidTransactionIssuingTime => {
-                write!(f, "The referenced input is created after transaction issuing time.")
-            }
-            Self::InvalidManaAmount => write!(f, "The mana amount is invalid."),
-            Self::InvalidBlockIssuanceCreditsAmount => write!(f, "The Block Issuance Credits amount is invalid."),
-            Self::InvalidRewardContextInput => write!(f, "Reward Context Input is invalid."),
-            Self::InvalidCommitmentContextInput => write!(f, "Commitment Context Input is invalid."),
-            Self::MissingStakingFeature => write!(
-                f,
-                "Staking Feature is not provided in account output when claiming rewards."
-            ),
-            Self::FailedToClaimStakingReward => write!(f, "Failed to claim staking reward."),
-            Self::FailedToClaimDelegationReward => write!(f, "Failed to claim delegation reward."),
-            Self::TransactionCapabilityNativeTokenBurningNotAllowed => write!(
-                f,
-                "Burning of native tokens is not allowed in the transaction capabilities."
-            ),
-            Self::TransactionCapabilityManaBurningNotAllowed => {
-                write!(f, "Burning of mana is not allowed in the transaction capabilities.")
-            }
-            Self::TransactionCapabilityAccountDestructionNotAllowed => write!(
-                f,
-                "Destruction of accounts is not allowed in the transaction capabilities."
-            ),
-            Self::TransactionCapabilityAnchorDestructionNotAllowed => write!(
-                f,
-                "Destruction of anchors is not allowed in the transaction capabilities."
-            ),
-            Self::TransactionCapabilityFoundryDestructionNotAllowed => write!(
-                f,
-                "Destruction of foundries is not allowed in the transaction capabilities."
-            ),
-            Self::TransactionCapabilityNftDestructionNotAllowed => {
-                write!(f, "Destruction of nfts is not allowed in the transaction capabilities.")
-            }
-            Self::SemanticValidationFailed => write!(
-                f,
-                "The semantic validation failed for a reason not covered by the previous variants."
-            ),
-        }
-    }
-}
+// impl fmt::Display for TransactionFailureReason {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             Self::InputUtxoAlreadySpent => write!(f, "The referenced UTXO was already spent."),
+//             Self::ConflictingWithAnotherTx => write!(
+//                 f,
+//                 "The transaction is conflicting with another transaction. Conflicting specifically means a double spend situation that both transactions pass all validation rules, eventually losing one(s) should have this reason."
+//             ),
+//             Self::InvalidReferencedUtxo => write!(f, "The referenced UTXO is invalid."),
+//             Self::InvalidTransaction => write!(f, "The transaction is invalid."),
+//             Self::SumInputsOutputsAmountMismatch => {
+//                 write!(f, "The sum of the inputs and output base token amount does not match.")
+//             }
+//             Self::InvalidUnlockBlockSignature => write!(f, "The unlock block signature is invalid."),
+//             Self::TimelockNotExpired => write!(f, "The configured timelock is not yet expired."),
+//             Self::InvalidNativeTokens => write!(f, "The given native tokens are invalid."),
+//             Self::StorageDepositReturnUnfulfilled => write!(
+//                 f,
+//                 "The return amount in a transaction is not fulfilled by the output side."
+//             ),
+//             Self::InvalidInputUnlock => write!(f, "An input unlock was invalid."),
+//             Self::SenderNotUnlocked => write!(
+//                 f,
+//                 "The output contains a Sender with an ident (address) which is not unlocked."
+//             ),
+//             Self::InvalidChainStateTransition => write!(f, "The chain state transition is invalid."),
+//             Self::InvalidTransactionIssuingTime => {
+//                 write!(f, "The referenced input is created after transaction issuing time.")
+//             }
+//             Self::InvalidManaAmount => write!(f, "The mana amount is invalid."),
+//             Self::InvalidBlockIssuanceCreditsAmount => write!(f, "The Block Issuance Credits amount is invalid."),
+//             Self::InvalidRewardContextInput => write!(f, "Reward Context Input is invalid."),
+//             Self::InvalidCommitmentContextInput => write!(f, "Commitment Context Input is invalid."),
+//             Self::MissingStakingFeature => write!(
+//                 f,
+//                 "Staking Feature is not provided in account output when claiming rewards."
+//             ),
+//             Self::FailedToClaimStakingReward => write!(f, "Failed to claim staking reward."),
+//             Self::FailedToClaimDelegationReward => write!(f, "Failed to claim delegation reward."),
+//             Self::TransactionCapabilityNativeTokenBurningNotAllowed => write!(
+//                 f,
+//                 "Burning of native tokens is not allowed in the transaction capabilities."
+//             ),
+//             Self::TransactionCapabilityManaBurningNotAllowed => {
+//                 write!(f, "Burning of mana is not allowed in the transaction capabilities.")
+//             }
+//             Self::TransactionCapabilityAccountDestructionNotAllowed => write!(
+//                 f,
+//                 "Destruction of accounts is not allowed in the transaction capabilities."
+//             ),
+//             Self::TransactionCapabilityAnchorDestructionNotAllowed => write!(
+//                 f,
+//                 "Destruction of anchors is not allowed in the transaction capabilities."
+//             ),
+//             Self::TransactionCapabilityFoundryDestructionNotAllowed => write!(
+//                 f,
+//                 "Destruction of foundries is not allowed in the transaction capabilities."
+//             ),
+//             Self::TransactionCapabilityNftDestructionNotAllowed => {
+//                 write!(f, "Destruction of nfts is not allowed in the transaction capabilities.")
+//             }
+//             Self::SemanticValidationFailed => write!(
+//                 f,
+//                 "The semantic validation failed for a reason not covered by the previous variants."
+//             ),
+//         }
+//     }
+// }
 
 impl TryFrom<u8> for TransactionFailureReason {
     type Error = Error;
