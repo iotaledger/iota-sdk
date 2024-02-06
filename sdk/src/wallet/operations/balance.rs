6ce7cb2cde28cd8e90f81be7af522c56bec10650
Alex Coats <alexandcoats@gmail.com>
2024-02-06 14:43:42 -0500

    only account/delegation

--- sdk/src/wallet/operations/balance.rs
+++ sdk/src/wallet/operations/balance.rs
@@ -69,6 +69,7 @@ where
                         output_id.transaction_id().slot_index(),
                         slot_index,
                     )?;
+                    // Add mana rewards
                     if let Ok(response) = self.client().get_output_mana_rewards(output_id, slot_index).await {
                         balance.mana.rewards += response.rewards;
                     }
@@ -101,6 +102,10 @@ where
                 Output::Delegation(delegation) => {
                     // Add amount
                     balance.base_coin.total += delegation.amount();
+                    // Add mana rewards
+                    if let Ok(response) = self.client().get_output_mana_rewards(output_id, slot_index).await {
+                        balance.mana.rewards += response.rewards;
+                    }
                     // Add storage deposit
                     balance.required_storage_deposit.delegation += storage_cost;
                     if !wallet_data.locked_outputs.contains(output_id) {
@@ -132,9 +137,6 @@ where
                             output_id.transaction_id().slot_index(),
                             slot_index,
                         )?;
-                        if let Ok(response) = self.client().get_output_mana_rewards(output_id, slot_index).await {
-                            balance.mana.rewards += response.rewards;
-                        }
 
                         // Add storage deposit
                         if output.is_basic() {
@@ -210,10 +212,6 @@ where
                                     output_id.transaction_id().slot_index(),
                                     slot_index,
                                 )?;
-                                if let Ok(response) = self.client().get_output_mana_rewards(output_id, slot_index).await
-                                {
-                                    balance.mana.rewards += response.rewards;
-                                }
 
                                 // Add storage deposit
                                 if output.is_basic() {
