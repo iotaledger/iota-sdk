2dcb9a9dea427bb982d698a44758f26706cb2f16
Thoralf Müller <thoralf.mue@gmail.com>
2024-02-14 11:31:05 +0100

    Make TaggedDataPayload work

--- bindings/python/CHANGELOG.md
+++ bindings/python/CHANGELOG.md
@@ -34,6 +34,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `NodeInfoProtocol.belowMaxDepth`;
 - `{NodeInfoProtocol, RentStructure}::as_dict()`;
 
+### Changed
+
+- Made the type of `TaggedDataPayload` required as first field in `__init__()`;
+
 ### Fixed
 
 - `TransactionOptions::as_dict()` for remainder_value_strategy;
--- bindings/python/examples/wallet/transaction_options.py
+++ bindings/python/examples/wallet/transaction_options.py
@@ -2,31 +2,33 @@ import os
 
 from dotenv import load_dotenv
 
-from iota_sdk import (RemainderValueStrategy, TaggedDataPayload,
+from iota_sdk import (RemainderValueStrategy, TaggedDataPayload, PayloadType,
                       TransactionOptions, Wallet, utf8_to_hex)
 
 load_dotenv()
 
 # This example sends a transaction with a tagged data payload.
 
-wallet = Wallet(os.environ['WALLET_DB_PATH'])
+TaggedDataPayload(PayloadType.TaggedData,
+                  utf8_to_hex("tag"), utf8_to_hex("data"))
+# wallet = Wallet(os.environ['WALLET_DB_PATH'])
 
-account = wallet.get_account('Alice')
+# account = wallet.get_account('Alice')
 
-# Sync account with the node
-response = account.sync()
+# # Sync account with the node
+# response = account.sync()
 
-if 'STRONGHOLD_PASSWORD' not in os.environ:
-    raise Exception(".env STRONGHOLD_PASSWORD is undefined, see .env.example")
+# if 'STRONGHOLD_PASSWORD' not in os.environ:
+#     raise Exception(".env STRONGHOLD_PASSWORD is undefined, see .env.example")
 
-wallet.set_stronghold_password(os.environ["STRONGHOLD_PASSWORD"])
+# wallet.set_stronghold_password(os.environ["STRONGHOLD_PASSWORD"])
 
-params = [{
-    "address": "rms1qpszqzadsym6wpppd6z037dvlejmjuke7s24hm95s9fg9vpua7vluaw60xu",
-    "amount": "1000000",
-}]
+# params = [{
+#     "address": "rms1qpszqzadsym6wpppd6z037dvlejmjuke7s24hm95s9fg9vpua7vluaw60xu",
+#     "amount": "1000000",
+# }]
 
-transaction = account.send_with_params(params, TransactionOptions(remainder_value_strategy=RemainderValueStrategy.ReuseAddress,
-                                                                  note="my first tx", tagged_data_payload=TaggedDataPayload(utf8_to_hex("tag"), utf8_to_hex("data"))))
-print(transaction)
-print(f'Block sent: {os.environ["EXPLORER_URL"]}/block/{transaction.blockId}')
+# transaction = account.send_with_params(params, TransactionOptions(remainder_value_strategy=RemainderValueStrategy.ReuseAddress,
+#                                                                   note="my first tx", tagged_data_payload=TaggedDataPayload(PayloadType.TaggedData, utf8_to_hex("tag"), utf8_to_hex("data"))))
+# print(transaction)
+# print(f'Block sent: {os.environ["EXPLORER_URL"]}/block/{transaction.blockId}')
