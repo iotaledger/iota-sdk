5b16d6f53e992215e2c27b812be4226e8f1d3c9a
shufps <shufps80@gmail.com>
2024-02-19 17:00:32 +0100

    cargo fmt

--- sdk/src/client/secret/ledger_nano.rs
+++ sdk/src/client/secret/ledger_nano.rs
@@ -16,8 +16,8 @@ use crypto::{
     },
 };
 use iota_ledger_nano::{
-    api::errors::APIError, get_app_config, get_buffer_size, get_ledger, get_opened_app, LedgerBIP32Index,
-    Packable as LedgerNanoPackable, TransportTypes,
+    api::constants::Protocols, api::errors::APIError, get_app_config, get_buffer_size, get_ledger, get_opened_app,
+    LedgerBIP32Index, Packable as LedgerNanoPackable, TransportTypes,
 };
 use packable::{error::UnexpectedEOF, unpacker::SliceUnpacker, Packable, PackableExt};
 use tokio::sync::Mutex;
@@ -159,7 +159,7 @@ impl SecretManage for LedgerSecretManager {
         let lock = self.mutex.lock().await;
 
         // get ledger
-        let ledger = get_ledger(coin_type, bip32_account, self.is_simulator).map_err(Error::from)?;
+        let ledger = get_ledger(coin_type, Protocols::Nova, bip32_account, self.is_simulator).map_err(Error::from)?;
         if ledger.is_debug_app() {
             ledger
                 .set_non_interactive_mode(self.non_interactive)
@@ -206,7 +206,7 @@ impl SecretManage for LedgerSecretManager {
         // Lock the mutex to prevent multiple simultaneous requests to a ledger.
         let lock = self.mutex.lock().await;
 
-        let ledger = get_ledger(coin_type, account_index, self.is_simulator).map_err(Error::from)?;
+        let ledger = get_ledger(coin_type, Protocols::Nova, account_index, self.is_simulator).map_err(Error::from)?;
         if ledger.is_debug_app() {
             ledger
                 .set_non_interactive_mode(self.non_interactive)
@@ -287,7 +287,7 @@ impl SecretManage for LedgerSecretManager {
         // lock the mutex to prevent multiple simultaneous requests to a ledger
         let lock = self.mutex.lock().await;
 
-        let ledger = get_ledger(coin_type, bip32_account, self.is_simulator).map_err(Error::from)?;
+        let ledger = get_ledger(coin_type, Protocols::Nova, bip32_account, self.is_simulator).map_err(Error::from)?;
         if ledger.is_debug_app() {
             ledger
                 .set_non_interactive_mode(self.non_interactive)
