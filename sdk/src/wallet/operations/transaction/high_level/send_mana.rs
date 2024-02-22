b5690f7fae670c3aacffefc7733cea67b2b7d3c9
Thibault Martinez <thibault@iota.org>
2024-02-22 11:05:29 +0100

    Add SendManaParams

--- sdk/src/wallet/operations/transaction/high_level/send_mana.rs
+++ sdk/src/wallet/operations/transaction/high_level/send_mana.rs
@@ -1,6 +1,8 @@
 // Copyright 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use serde::{Deserialize, Serialize};
+
 use crate::{
     client::{api::PreparedTransactionData, secret::SecretManage},
     types::block::{
@@ -10,12 +12,22 @@ use crate::{
             BasicOutputBuilder,
         },
     },
-    utils::ConvertTo,
+    utils::serde::string,
     wallet::{
         operations::transaction::{prepare_output::ReturnStrategy, TransactionOptions, TransactionWithMetadata},
         Wallet,
     },
 };
+
+/// Params for `send_mana()`.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SendManaParams {
+    #[serde(with = "string")]
+    mana: u64,
+    address: Bech32Address,
+    return_strategy: Option<ReturnStrategy>,
+}
 
 impl<S: 'static + SecretManage> Wallet<S>
 where
@@ -24,34 +36,27 @@ where
 {
     pub async fn send_mana(
         &self,
-        mana: u64,
-        address: impl ConvertTo<Bech32Address>,
-        return_strategy: impl Into<Option<ReturnStrategy>> + Send,
+        params: SendManaParams,
         options: impl Into<Option<TransactionOptions>> + Send,
     ) -> crate::wallet::Result<TransactionWithMetadata> {
         let options = options.into();
-        let prepared_transaction = self
-            .prepare_send_mana(mana, address, return_strategy, options.clone())
-            .await?;
+        let prepared_transaction = self.prepare_send_mana(params, options.clone()).await?;
 
         self.sign_and_submit_transaction(prepared_transaction, options).await
     }
 
     pub async fn prepare_send_mana(
         &self,
-        mana: u64,
-        address: impl ConvertTo<Bech32Address>,
-        return_strategy: impl Into<Option<ReturnStrategy>> + Send,
+        params: SendManaParams,
         options: impl Into<Option<TransactionOptions>> + Send,
     ) -> crate::wallet::Result<PreparedTransactionData> {
         log::debug!("[TRANSACTION] prepare_send_mana");
-        let return_strategy = return_strategy.into().unwrap_or_default();
-        let options = options.into();
+        let return_strategy = params.return_strategy.unwrap_or_default();
         let storage_score_params = self.client().get_storage_score_parameters().await?;
 
         let mut output_builder = BasicOutputBuilder::new_with_minimum_amount(storage_score_params)
-            .with_mana(mana)
-            .add_unlock_condition(AddressUnlockCondition::new(address.convert()?));
+            .with_mana(params.mana)
+            .add_unlock_condition(AddressUnlockCondition::new(params.address));
 
         if let ReturnStrategy::Return = return_strategy {
             output_builder = output_builder.add_unlock_condition(StorageDepositReturnUnlockCondition::new(
