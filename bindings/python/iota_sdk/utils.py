947a8eae97de3e5fc84c30d76194b5d0be0715cf
Thibault Martinez <thibault@iota.org>
2024-02-27 11:28:55 +0100

    python

--- bindings/python/iota_sdk/utils.py
+++ bindings/python/iota_sdk/utils.py
@@ -241,10 +241,10 @@ class Utils:
 
     @staticmethod
     def verify_transaction_semantic(
-            transaction: Transaction, inputs: List[InputSigningData], protocol_parameters: ProtocolParameters, unlocks: Optional[List[Unlock]] = None, mana_rewards: Optional[dict[OutputId, int]] = None) -> str:
+            transaction: Transaction, inputs: List[InputSigningData], protocol_parameters: ProtocolParameters, unlocks: Optional[List[Unlock]] = None, mana_rewards: Optional[dict[OutputId, int]] = None):
         """Verifies the semantic of a transaction.
         """
-        return _call_method('verifyTransactionSemantic', {
+        _call_method('verifyTransactionSemantic', {
             'transaction': transaction,
             'inputs': inputs,
             'unlocks': unlocks,
