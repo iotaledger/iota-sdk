7e3b82db22153516c3f831ae8ed234d630495d63
Alexander Schmidt <alexander.schmidt@iota.org>
2024-02-29 18:59:34 +0100

    the example

---
+++ sdk/examples/client/node_api_core/get_validators.rs
@@ -0,0 +1,41 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! This example returns the health of the node by querying its `/health` endpoint.
+//!
+//! Rename `.env.example` to `.env` first, then run the command:
+//! ```sh
+//! cargo run --release --all-features --example node_api_core_get_validators [LIST_INDEX] [NODE_URL]
+//! ```
+
+use iota_sdk::client::{Client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // If not provided we use the default node from the `.env` file.
+    dotenvy::dotenv().ok();
+
+    let list_index = std::env::args().nth(1).map(|s| s.parse::<u32>().unwrap());
+
+    // Take the node URL from command line argument or use one from env as default.
+    let node_url = std::env::args()
+        .nth(2)
+        .unwrap_or_else(|| std::env::var("NODE_URL").expect("NODE_URL not set"));
+
+    // Create a node client.
+    let client = Client::builder()
+        .with_node(&node_url)?
+        .with_ignore_node_health()
+        .finish()
+        .await?;
+
+    let slot_index = client.get_info().await?.node_info.status.latest_finalized_slot;
+    let cursor = list_index.map(|i| format!("{slot_index},{i}"));
+
+    // Get validators.
+    let res = client.get_validators(1, cursor).await?;
+
+    println!("{res:?}");
+
+    Ok(())
+}
