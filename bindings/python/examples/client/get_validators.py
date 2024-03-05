675f8fb6c3e121d6c38842828d439385341ac808
Alexander Schmidt <alexander.schmidt@iota.org>
2024-03-05 10:35:23 +0100

    python: update example

--- bindings/python/examples/client/get_validators.py
+++ bindings/python/examples/client/get_validators.py
@@ -1,6 +1,7 @@
 import dataclasses
 import json
 import os
+import sys
 
 from dotenv import load_dotenv
 from iota_sdk import Client
@@ -8,13 +9,16 @@ from iota_sdk import Client
 load_dotenv()
 
 node_url = os.environ.get('NODE_URL', 'https://api.testnet.shimmer.network')
+page_size = 1
+cursor = ""
+
+if len(sys.argv) > 1:
+    page_size = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        cursor = sys.argv[2]
 
 # Create a Client instance
 client = Client(nodes=[node_url])
 
-list_index = 1
-slot_index = client.get_node_info().info.status.latest_finalized_slot
-cursor = str(slot_index) + "," + str(list_index)
-
-validators_response = client.get_validators(1, cursor)
+validators_response = client.get_validators(page_size, cursor)
 print(f'{json.dumps(dataclasses.asdict(validators_response), indent=4)}')
