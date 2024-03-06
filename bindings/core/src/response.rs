4aac8377acd35ed018784546227c2001a497e7a9
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2024-03-06 18:01:55 +0100

    Cleanup bindings send methods (#2137)
    * Cleanup bindings send methods
    
    * format
    
    * Keep prepare_send
    
    * Revert comment change
    
    ---------
    
    Co-authored-by: DaughterOfMars <alexandcoats@gmail.com>

--- bindings/core/src/response.rs
+++ bindings/core/src/response.rs
@@ -271,12 +271,12 @@ pub enum Response {
     /// - [`VerifySecp256k1EcdsaSignature`](crate::method::UtilsMethod::VerifySecp256k1EcdsaSignature)
     Bool(bool),
     /// Response for:
-    /// - [`Backup`](crate::method::WalletMethod::Backup),
+    /// - [`BackupToStrongholdSnapshot`](crate::method::WalletMethod::BackupToStrongholdSnapshot),
     /// - [`ClearListeners`](crate::method::WalletMethod::ClearListeners)
     /// - [`ClearStrongholdPassword`](crate::method::WalletMethod::ClearStrongholdPassword),
     /// - [`DeregisterParticipationEvent`](crate::method::WalletMethod::DeregisterParticipationEvent),
     /// - [`EmitTestEvent`](crate::method::WalletMethod::EmitTestEvent),
-    /// - [`RestoreBackup`](crate::method::WalletMethod::RestoreBackup),
+    /// - [`RestoreFromStrongholdSnapshot`](crate::method::WalletMethod::RestoreFromStrongholdSnapshot),
     /// - [`SetAlias`](crate::method::WalletMethod::SetAlias),
     /// - [`SetClientOptions`](crate::method::WalletMethod::SetClientOptions),
     /// - [`SetDefaultSyncOptions`](crate::method::WalletMethod::SetDefaultSyncOptions),
@@ -326,10 +326,10 @@ pub enum Response {
     /// - [`PrepareMeltNativeToken`](crate::method::WalletMethod::PrepareMeltNativeToken)
     /// - [`PrepareMintNativeToken`](crate::method::WalletMethod::PrepareMintNativeToken),
     /// - [`PrepareMintNfts`](crate::method::WalletMethod::PrepareMintNfts),
-    /// - [`PrepareSend`](crate::method::WalletMethod::PrepareSend),
     /// - [`PrepareSendMana`](crate::method::WalletMethod::PrepareSendMana),
     /// - [`PrepareSendNativeTokens`](crate::method::WalletMethod::PrepareSendNativeTokens),
     /// - [`PrepareSendNft`](crate::method::WalletMethod::PrepareSendNft),
+    /// - [`PrepareSend`](crate::method::WalletMethod::PrepareSend),
     /// - [`PrepareStopParticipating`](crate::method::WalletMethod::PrepareStopParticipating)
     /// - [`PrepareSendOutputs`](crate::method::WalletMethod::PrepareSendOutputs)
     /// - [`PrepareVote`](crate::method::WalletMethod::PrepareVote)
@@ -358,9 +358,6 @@ pub enum Response {
     /// - [`Sync`](crate::method::WalletMethod::Sync)
     Balance(Balance),
     /// Response for:
-    /// - [`ClaimOutputs`](crate::method::WalletMethod::ClaimOutputs)
-    /// - [`Send`](crate::method::WalletMethod::Send)
-    /// - [`SendOutputs`](crate::method::WalletMethod::SendOutputs)
     /// - [`SignAndSubmitTransaction`](crate::method::WalletMethod::SignAndSubmitTransaction)
     /// - [`SubmitAndStoreTransaction`](crate::method::WalletMethod::SubmitAndStoreTransaction)
     SentTransaction(TransactionWithMetadataDto),
