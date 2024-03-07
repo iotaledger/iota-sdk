8e7d26c64f0d4c63b0343ed89ec94b9b048d21a2
Thibault Martinez <thibault@iota.org>
2024-03-07 11:05:34 +0100

    python TransactionFailureReason

--- bindings/python/iota_sdk/types/transaction_metadata.py
+++ bindings/python/iota_sdk/types/transaction_metadata.py
@@ -31,145 +31,147 @@ class TransactionFailureReason(Enum):
     """
     Null = 0
     ConflictRejected = 1
-    InputAlreadySpent = 2
-    InputCreationAfterTxCreation = 3
-    UnlockSignatureInvalid = 4
-    ChainAddressUnlockInvalid = 5
-    DirectUnlockableAddressUnlockInvalid = 6
-    MultiAddressUnlockInvalid = 7
-    CommitmentInputReferenceInvalid = 8
-    BicInputReferenceInvalid = 9
-    RewardInputReferenceInvalid = 10
-    StakingRewardCalculationFailure = 11
-    DelegationRewardCalculationFailure = 12
-    InputOutputBaseTokenMismatch = 13
-    ManaOverflow = 14
-    InputOutputManaMismatch = 15
-    ManaDecayCreationIndexExceedsTargetIndex = 16
-    NativeTokenSumUnbalanced = 17
-    SimpleTokenSchemeMintedMeltedTokenDecrease = 18
-    SimpleTokenSchemeMintingInvalid = 19
-    SimpleTokenSchemeMeltingInvalid = 20
-    SimpleTokenSchemeMaximumSupplyChanged = 21
-    SimpleTokenSchemeGenesisInvalid = 22
-    MultiAddressLengthUnlockLengthMismatch = 23
-    MultiAddressUnlockThresholdNotReached = 24
-    SenderFeatureNotUnlocked = 25
-    IssuerFeatureNotUnlocked = 26
-    StakingRewardInputMissing = 27
-    StakingCommitmentInputMissing = 28
-    StakingRewardClaimingInvalid = 29
-    StakingFeatureRemovedBeforeUnbonding = 30
-    StakingFeatureModifiedBeforeUnbonding = 31
-    StakingStartEpochInvalid = 32
-    StakingEndEpochTooEarly = 33
-    BlockIssuerCommitmentInputMissing = 34
-    BlockIssuanceCreditInputMissing = 35
-    BlockIssuerNotExpired = 36
-    BlockIssuerExpiryTooEarly = 37
-    ManaMovedOffBlockIssuerAccount = 38
-    AccountLocked = 39
-    TimelockCommitmentInputMissing = 40
-    TimelockNotExpired = 41
-    ExpirationCommitmentInputMissing = 42
-    ExpirationNotUnlockable = 43
-    ReturnAmountNotFulFilled = 44
-    NewChainOutputHasNonZeroedId = 45
-    ChainOutputImmutableFeaturesChanged = 46
-    ImplicitAccountDestructionDisallowed = 47
-    MultipleImplicitAccountCreationAddresses = 48
-    AccountInvalidFoundryCounter = 49
-    AnchorInvalidStateTransition = 50
-    AnchorInvalidGovernanceTransition = 51
-    FoundryTransitionWithoutAccount = 52
-    FoundrySerialInvalid = 53
-    DelegationCommitmentInputMissing = 54
-    DelegationRewardInputMissing = 54
-    DelegationRewardsClaimingInvalid = 56
-    DelegationOutputTransitionedTwice = 57
-    DelegationModified = 58
-    DelegationStartEpochInvalid = 59
-    DelegationAmountMismatch = 60
-    DelegationEndEpochNotZero = 61
-    DelegationEndEpochInvalid = 62
-    CapabilitiesNativeTokenBurningNotAllowed = 63
-    CapabilitiesManaBurningNotAllowed = 64
-    CapabilitiesAccountDestructionNotAllowed = 65
-    CapabilitiesAnchorDestructionNotAllowed = 66
-    CapabilitiesFoundryDestructionNotAllowed = 67
-    CapabilitiesNftDestructionNotAllowed = 68
+    Orphaned = 2
+    InputAlreadySpent = 3
+    InputCreationAfterTxCreation = 4
+    UnlockSignatureInvalid = 5
+    ChainAddressUnlockInvalid = 6
+    DirectUnlockableAddressUnlockInvalid = 7
+    MultiAddressUnlockInvalid = 8
+    CommitmentInputReferenceInvalid = 9
+    BicInputReferenceInvalid = 10
+    RewardInputReferenceInvalid = 11
+    StakingRewardCalculationFailure = 12
+    DelegationRewardCalculationFailure = 13
+    InputOutputBaseTokenMismatch = 14
+    ManaOverflow = 15
+    InputOutputManaMismatch = 16
+    ManaDecayCreationIndexExceedsTargetIndex = 17
+    NativeTokenSumUnbalanced = 18
+    SimpleTokenSchemeMintedMeltedTokenDecrease = 19
+    SimpleTokenSchemeMintingInvalid = 20
+    SimpleTokenSchemeMeltingInvalid = 21
+    SimpleTokenSchemeMaximumSupplyChanged = 22
+    SimpleTokenSchemeGenesisInvalid = 23
+    MultiAddressLengthUnlockLengthMismatch = 24
+    MultiAddressUnlockThresholdNotReached = 25
+    SenderFeatureNotUnlocked = 26
+    IssuerFeatureNotUnlocked = 27
+    StakingRewardInputMissing = 28
+    StakingCommitmentInputMissing = 29
+    StakingRewardClaimingInvalid = 30
+    StakingFeatureRemovedBeforeUnbonding = 31
+    StakingFeatureModifiedBeforeUnbonding = 32
+    StakingStartEpochInvalid = 33
+    StakingEndEpochTooEarly = 34
+    BlockIssuerCommitmentInputMissing = 35
+    BlockIssuanceCreditInputMissing = 36
+    BlockIssuerNotExpired = 37
+    BlockIssuerExpiryTooEarly = 38
+    ManaMovedOffBlockIssuerAccount = 39
+    AccountLocked = 40
+    TimelockCommitmentInputMissing = 41
+    TimelockNotExpired = 42
+    ExpirationCommitmentInputMissing = 43
+    ExpirationNotUnlockable = 44
+    ReturnAmountNotFulFilled = 45
+    NewChainOutputHasNonZeroedId = 46
+    ChainOutputImmutableFeaturesChanged = 47
+    ImplicitAccountDestructionDisallowed = 48
+    MultipleImplicitAccountCreationAddresses = 49
+    AccountInvalidFoundryCounter = 40
+    AnchorInvalidStateTransition = 51
+    AnchorInvalidGovernanceTransition = 52
+    FoundryTransitionWithoutAccount = 53
+    FoundrySerialInvalid = 54
+    DelegationCommitmentInputMissing = 55
+    DelegationRewardInputMissing = 56
+    DelegationRewardsClaimingInvalid = 57
+    DelegationOutputTransitionedTwice = 58
+    DelegationModified = 59
+    DelegationStartEpochInvalid = 60
+    DelegationAmountMismatch = 61
+    DelegationEndEpochNotZero = 62
+    DelegationEndEpochInvalid = 63
+    CapabilitiesNativeTokenBurningNotAllowed = 64
+    CapabilitiesManaBurningNotAllowed = 65
+    CapabilitiesAccountDestructionNotAllowed = 66
+    CapabilitiesAnchorDestructionNotAllowed = 67
+    CapabilitiesFoundryDestructionNotAllowed = 68
+    CapabilitiesNftDestructionNotAllowed = 69
     SemanticValidationFailed = 255
 
     def __str__(self):
         return {
             0: "Null.",
             1: "Transaction was conflicting and was rejected.",
-            2: "Input already spent.",
-            3: "Input creation slot after tx creation slot.",
-            4: "Signature in unlock is invalid.",
-            5: "Invalid unlock for chain address.",
-            6: "Invalid unlock for direct unlockable address.",
-            7: "Invalid unlock for multi address.",
-            8: "Commitment input references an invalid or non-existent commitment.",
-            9: "BIC input reference cannot be loaded.",
-            10: "Reward input does not reference a staking account or a delegation output.",
-            11: "Staking rewards could not be calculated due to storage issues or overflow.",
-            12: "Delegation rewards could not be calculated due to storage issues or overflow.",
-            13: "Inputs and outputs do not spend/deposit the same amount of base tokens.",
-            14: "Under- or overflow in Mana calculations.",
-            15: "Inputs and outputs do not contain the same amount of Mana.",
-            16: "Mana decay creation slot/epoch index exceeds target slot/epoch index.",
-            17: "Native token sums are unbalanced.",
-            18: "Simple token scheme minted/melted value decreased.",
-            19: "Simple token scheme's minted tokens did not increase by the minted amount or melted tokens changed.",
-            20: "Simple token scheme's melted tokens did not increase by the melted amount or minted tokens changed.",
-            21: "Simple token scheme's maximum supply cannot change during transition.",
-            22: "Newly created simple token scheme's melted tokens are not zero or minted tokens do not equal native token amount in transaction.",
-            23: "Multi address length and multi unlock length do not match.",
-            24: "Multi address unlock threshold not reached.",
-            25: "Sender feature is not unlocked.",
-            26: "Issuer feature is not unlocked.",
-            27: "Staking feature removal or resetting requires a reward input.",
-            28: "Staking feature validation requires a commitment input.",
-            29: "Staking feature must be removed or reset in order to claim rewards.",
-            30: "Staking feature can only be removed after the unbonding period.",
-            31: "Staking start epoch, fixed cost and staked amount cannot be modified while bonded.",
-            32: "Staking start epoch must be the epoch of the transaction.",
-            33: "Staking end epoch must be set to the transaction epoch plus the unbonding period.",
-            34: "Commitment input missing for block issuer feature.",
-            35: "Block issuance credit input missing for account with block issuer feature.",
-            36: "Block issuer feature has not expired.",
-            37: "Block issuer feature expiry set too early.",
-            38: "Mana cannot be moved off block issuer accounts except with manalocks.",
-            39: "Account is locked due to negative block issuance credits.",
-            40: "Transaction's containing a timelock condition require a commitment input.",
-            41: "Timelock not expired.",
-            42: "Transaction's containing an expiration condition require a commitment input.",
-            43: "Expiration unlock condition cannot be unlocked.",
-            44: "Return amount not fulfilled.",
-            45: "New chain output has non-zeroed ID.",
-            46: "Immutable features in chain output modified during transition.",
-            47: "Cannot destroy implicit account; must be transitioned to account.",
-            48: "Multiple implicit account creation addresses on the input side.",
-            49: "Foundry counter in account decreased or did not increase by the number of new foundries.",
-            50: "Anchor has an invalid state transition.",
-            51: "Anchor has an invalid governance transition.",
-            52: "Foundry output transitioned without accompanying account on input or output side.",
-            53: "Foundry output serial number is invalid.",
-            54: "Delegation output validation requires a commitment input.",
-            55: "Delegation output cannot be destroyed without a reward input.",
-            56: "Invalid delegation mana rewards claiming.",
-            57: "Delegation output attempted to be transitioned twice.",
-            58: "Delegated amount, validator ID and start epoch cannot be modified.",
-            59: "Invalid start epoch.",
-            60: "Delegated amount does not match amount.",
-            61: "End epoch must be set to zero at output genesis.",
-            62: "Delegation end epoch does not match current epoch.",
-            63: "Native token burning is not allowed by the transaction capabilities.",
-            64: "Mana burning is not allowed by the transaction capabilities.",
-            65: "Account destruction is not allowed by the transaction capabilities.",
-            66: "Anchor destruction is not allowed by the transaction capabilities.",
-            67: "Foundry destruction is not allowed by the transaction capabilities.",
-            68: "NFT destruction is not allowed by the transaction capabilities.",
+            2: "Transaction was orphaned.",
+            3: "Input already spent.",
+            4: "Input creation slot after tx creation slot.",
+            5: "Signature in unlock is invalid.",
+            6: "Invalid unlock for chain address.",
+            7: "Invalid unlock for direct unlockable address.",
+            8: "Invalid unlock for multi address.",
+            9: "Commitment input references an invalid or non-existent commitment.",
+            10: "BIC input reference cannot be loaded.",
+            11: "Reward input does not reference a staking account or a delegation output.",
+            12: "Staking rewards could not be calculated due to storage issues or overflow.",
+            13: "Delegation rewards could not be calculated due to storage issues or overflow.",
+            14: "Inputs and outputs do not spend/deposit the same amount of base tokens.",
+            15: "Under- or overflow in Mana calculations.",
+            16: "Inputs and outputs do not contain the same amount of Mana.",
+            17: "Mana decay creation slot/epoch index exceeds target slot/epoch index.",
+            18: "Native token sums are unbalanced.",
+            19: "Simple token scheme minted/melted value decreased.",
+            20: "Simple token scheme's minted tokens did not increase by the minted amount or melted tokens changed.",
+            21: "Simple token scheme's melted tokens did not increase by the melted amount or minted tokens changed.",
+            22: "Simple token scheme's maximum supply cannot change during transition.",
+            23: "Newly created simple token scheme's melted tokens are not zero or minted tokens do not equal native token amount in transaction.",
+            24: "Multi address length and multi unlock length do not match.",
+            25: "Multi address unlock threshold not reached.",
+            26: "Sender feature is not unlocked.",
+            27: "Issuer feature is not unlocked.",
+            28: "Staking feature removal or resetting requires a reward input.",
+            29: "Staking feature validation requires a commitment input.",
+            30: "Staking feature must be removed or reset in order to claim rewards.",
+            31: "Staking feature can only be removed after the unbonding period.",
+            32: "Staking start epoch, fixed cost and staked amount cannot be modified while bonded.",
+            33: "Staking start epoch must be the epoch of the transaction.",
+            34: "Staking end epoch must be set to the transaction epoch plus the unbonding period.",
+            35: "Commitment input missing for block issuer feature.",
+            36: "Block issuance credit input missing for account with block issuer feature.",
+            37: "Block issuer feature has not expired.",
+            38: "Block issuer feature expiry set too early.",
+            39: "Mana cannot be moved off block issuer accounts except with manalocks.",
+            40: "Account is locked due to negative block issuance credits.",
+            41: "Transaction's containing a timelock condition require a commitment input.",
+            42: "Timelock not expired.",
+            43: "Transaction's containing an expiration condition require a commitment input.",
+            44: "Expiration unlock condition cannot be unlocked.",
+            45: "Return amount not fulfilled.",
+            46: "New chain output has non-zeroed ID.",
+            47: "Immutable features in chain output modified during transition.",
+            48: "Cannot destroy implicit account; must be transitioned to account.",
+            49: "Multiple implicit account creation addresses on the input side.",
+            50: "Foundry counter in account decreased or did not increase by the number of new foundries.",
+            51: "Anchor has an invalid state transition.",
+            52: "Anchor has an invalid governance transition.",
+            53: "Foundry output transitioned without accompanying account on input or output side.",
+            54: "Foundry output serial number is invalid.",
+            55: "Delegation output validation requires a commitment input.",
+            56: "Delegation output cannot be destroyed without a reward input.",
+            57: "Invalid delegation mana rewards claiming.",
+            58: "Delegation output attempted to be transitioned twice.",
+            59: "Delegated amount, validator ID and start epoch cannot be modified.",
+            60: "Invalid start epoch.",
+            61: "Delegated amount does not match amount.",
+            62: "End epoch must be set to zero at output genesis.",
+            63: "Delegation end epoch does not match current epoch.",
+            64: "Native token burning is not allowed by the transaction capabilities.",
+            65: "Mana burning is not allowed by the transaction capabilities.",
+            66: "Account destruction is not allowed by the transaction capabilities.",
+            67: "Anchor destruction is not allowed by the transaction capabilities.",
+            68: "Foundry destruction is not allowed by the transaction capabilities.",
+            69: "NFT destruction is not allowed by the transaction capabilities.",
             255: "Semantic validation failed.",
         }[self.value]
