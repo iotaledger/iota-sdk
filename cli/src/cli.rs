45c4c7f0c3ed495a1b3509f0358f4f2f7d581e7a
Alexander Schmidt <alexander.schmidt@iota.org>
2024-03-11 10:56:37 +0100

    fix

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -337,7 +337,15 @@ pub async fn new_wallet(cli: Cli) -> Result<Option<Wallet>, Error> {
             Some(wallet)
         } else {
             // init new wallet with default init parameters
-            let init_params = InitParameters::default();
+            let mut init_params = InitParameters::default();
+
+            if let Some(stronghold_snapshot_path) = std::env::var("STRONGHOLD_SNAPSHOT_PATH").ok() {
+                init_params.stronghold_snapshot_path = stronghold_snapshot_path;
+            }
+            if let Some(node_url) = std::env::var("NODE_URL").ok() {
+                init_params.node_url = node_url;
+            }
+
             let snapshot_path = Path::new(&init_params.stronghold_snapshot_path);
             if !snapshot_path.exists() {
                 if get_decision("Create a new wallet with default parameters?")? {
