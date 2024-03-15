65115650c89868d2636c1eaa36e9bedb3ff2ba57
Alexander Schmidt <alexander.schmidt@iota.org>
2024-03-15 14:22:17 +0100

    no std

--- sdk/src/types/block/capabilities.rs
+++ sdk/src/types/block/capabilities.rs
@@ -62,7 +62,7 @@ impl<Flag> core::fmt::Debug for Capabilities<Flag> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.debug_struct("Capabilities")
             .field("bytes", &self.bytes)
-            .field("flag", &std::any::type_name::<Flag>().rsplit_once("::").unwrap().1)
+            .field("flag", &core::any::type_name::<Flag>().rsplit_once("::").unwrap().1)
             .finish()
     }
 }
