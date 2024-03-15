e0c26e874d1b09f7c5cdb103a2cde4d0914796df
Alexander Schmidt <alexander.schmidt@iota.org>
2024-03-15 15:25:02 +0100

    rm stronghold methods

--- sdk/src/wallet/core/operations/mod.rs
+++ sdk/src/wallet/core/operations/mod.rs
@@ -3,10 +3,6 @@
 
 pub(crate) mod background_syncing;
 pub(crate) mod client;
-#[cfg(feature = "ledger_nano")]
-pub(crate) mod ledger_nano;
 pub(crate) mod storage;
-#[cfg(feature = "stronghold")]
-pub(crate) mod stronghold;
 #[cfg(feature = "stronghold")]
 pub(crate) mod stronghold_backup;
