5ab53402ef44f5152216bd8b2e0bde35aef5a341
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2025-04-24 09:36:52 +0200

    Update bindings/python/iota_sdk/secret_manager/secret_manager.py
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- bindings/python/iota_sdk/secret_manager/secret_manager.py
+++ bindings/python/iota_sdk/secret_manager/secret_manager.py
@@ -241,7 +241,7 @@ class SecretManager():
         })
 
     def get_seed(self) -> HexStr:
-        """Get the seed from the Stronghold vault, that was stored by `StoreMnemonic`.
+        """Get the seed from the Stronghold vault, that was stored by `store_mnemonic `.
         """
         return self._call_method('getSeed')
 
